19abd73c65c51007729a0987cbd08a2e83ab89d9
ayusht2810 <ayush.thakur@brightscout.com>
2023-03-23 15:32:37 +0530

    [MI-2915] Add test cases for message_hooks.go

--- server/testutils/data.go
+++ server/testutils/data.go
@@ -16,3 +16,53 @@ func GetInternalServerAppError(errorMsg string) *model.AppError {
 func GetID() string {
 	return "sfmq19kpztg5iy47ebe51hb31w"
 }
+
+func GetChannelID() string {
+	return "bnqnzipmnir4zkkj95ggba5pde"
+}
+
+func GetPost() *model.Post {
+	return &model.Post{
+		Id:        GetID(),
+		FileIds:   model.StringArray{GetID()},
+		ChannelId: GetChannelID(),
+		UserId:    GetID(),
+	}
+}
+
+func GetChannel(channelType model.ChannelType) *model.Channel {
+	return &model.Channel{
+		Id:   GetChannelID(),
+		Type: channelType,
+	}
+}
+
+func GetChannelMembers(count int) model.ChannelMembers {
+	channelMembers := model.ChannelMembers{}
+
+	for i := 0; i < count; i++ {
+		channelMembers = append(channelMembers, model.ChannelMember{
+			UserId:    GetID(),
+			ChannelId: GetChannelID(),
+		})
+	}
+
+	return channelMembers
+}
+
+func GetUser(role, email string) *model.User {
+	return &model.User{
+		Id:       GetID(),
+		Username: "test-user",
+		Roles:    role,
+		Email:    email,
+	}
+}
+
+func GetReaction() *model.Reaction {
+	return &model.Reaction{
+		UserId:    GetID(),
+		PostId:    GetID(),
+		ChannelId: GetChannelID(),
+	}
+}
