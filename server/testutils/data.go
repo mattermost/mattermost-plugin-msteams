0eb10e17c6a48341441c64369eb7307fead5dfff
Ayush Thakur <100013900+ayusht2810@users.noreply.github.com>
2023-03-24 14:05:15 +0530

    Added unit test cases for message_hooks.go file (#17)
    * [MI-2901] Add test cases for store.go
    
    * [MI-2901] Update the strcutre of test cases
    
    * [MI-2915] Add test cases for message_hooks.go
    
    * [MI-2915] Minor fixes

---
+++ server/testutils/data.go
@@ -0,0 +1,67 @@
+package testutils
+
+import (
+	"net/http"
+
+	"github.com/mattermost/mattermost-server/v6/model"
+)
+
+func GetInternalServerAppError(errorMsg string) *model.AppError {
+	return &model.AppError{
+		StatusCode:    http.StatusInternalServerError,
+		DetailedError: errorMsg,
+	}
+}
+
+func GetID() string {
+	return "sfmq19kpztg5iy47ebe51hb31w"
+}
+
+func GetChannelID() string {
+	return "bnqnzipmnir4zkkj95ggba5pde"
+}
+
+func GetPost() *model.Post {
+	return &model.Post{
+		Id:        GetID(),
+		FileIds:   model.StringArray{GetID()},
+		ChannelId: GetChannelID(),
+		UserId:    GetID(),
+	}
+}
+
+func GetChannel(channelType model.ChannelType) *model.Channel {
+	return &model.Channel{
+		Id:   GetChannelID(),
+		Type: channelType,
+	}
+}
+
+func GetChannelMembers(count int) model.ChannelMembers {
+	channelMembers := model.ChannelMembers{}
+	for i := 0; i < count; i++ {
+		channelMembers = append(channelMembers, model.ChannelMember{
+			UserId:    GetID(),
+			ChannelId: GetChannelID(),
+		})
+	}
+
+	return channelMembers
+}
+
+func GetUser(role, email string) *model.User {
+	return &model.User{
+		Id:       GetID(),
+		Username: "test-user",
+		Roles:    role,
+		Email:    email,
+	}
+}
+
+func GetReaction() *model.Reaction {
+	return &model.Reaction{
+		UserId:    GetID(),
+		PostId:    GetID(),
+		ChannelId: GetChannelID(),
+	}
+}
