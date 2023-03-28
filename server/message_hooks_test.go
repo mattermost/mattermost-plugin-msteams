135ad4adf89813e5f1462e8a857c5fc4afcb0178
ayusht2810 <ayush.thakur@brightscout.com>
2023-03-28 14:09:01 +0530

    Add unit test cases for message_hooks.go file

--- server/message_hooks_test.go
+++ server/message_hooks_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/mattermost/mattermost-plugin-msteams-sync/server/msteams"
 	clientmocks "github.com/mattermost/mattermost-plugin-msteams-sync/server/msteams/mocks"
 	storemocks "github.com/mattermost/mattermost-plugin-msteams-sync/server/store/mocks"
 	"github.com/mattermost/mattermost-plugin-msteams-sync/server/store/storemodels"
@@ -175,6 +176,405 @@ func TestReactionHasBeenAdded(t *testing.T) {
 			test.SetupStore(p.store.(*storemocks.Store))
 			test.SetupClient(p.msteamsAppClient.(*clientmocks.Client), p.clientBuilderWithToken("", "", nil, nil).(*clientmocks.Client))
 			p.ReactionHasBeenAdded(&plugin.Context{}, testutils.GetReaction())
+		})
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	for _, test := range []struct {
+		Name          string
+		SetupAPI      func(*plugintest.API)
+		SetupStore    func(*storemocks.Store)
+		SetupClient   func(*clientmocks.Client, *clientmocks.Client)
+		ExpectedError string
+	}{
+		{
+			Name:     "Update: Unable to get the client",
+			SetupAPI: func(api *plugintest.API) {},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(nil, nil).Times(1)
+				store.On("GetTokenForMattermostUser", "bot-user-id").Return(nil, nil).Times(1)
+			},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "not connected user",
+		},
+		{
+			Name: "Update: Unable to get the post info",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogError", "Error updating post", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(nil, testutils.GetInternalServerAppError("unable to get the post info")).Times(1)
+			},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "unable to get the post info",
+		},
+		{
+			Name: "Update: Post info is nil",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogError", "Error updating post, post not found.")
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(nil, nil).Times(1)
+			},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "post not found",
+		},
+		{
+			Name: "Update: Unable to update the message",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogWarn", "Error updating the post", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockMSTeamsID",
+				}, nil).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("UpdateMessage", "mockTeamsTeamID", testutils.GetChannelID(), "", "mockMSTeamsID", "mockMessage").Return(errors.New("unable to update the message")).Times(1)
+			},
+			ExpectedError: "unable to update the message",
+		},
+		{
+			Name: "Update: Unable to get the updated message",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogWarn", "Error updating the msteams/mattermost post link metadata", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockMSTeamsID",
+				}, nil).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("UpdateMessage", "mockTeamsTeamID", testutils.GetChannelID(), "", "mockMSTeamsID", "mockMessage").Return(nil).Times(1)
+				uclient.On("GetMessage", "mockTeamsTeamID", testutils.GetChannelID(), "mockMSTeamsID").Return(nil, errors.New("unable to get the updated message")).Times(1)
+			},
+		},
+		{
+			Name: "Update: Unable to store the link posts",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogWarn", "Error updating the msteams/mattermost post link metadata", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockMSTeamsID",
+				}, nil).Times(1)
+				store.On("LinkPosts", storemodels.PostInfo{
+					MattermostID:   testutils.GetID(),
+					MSTeamsChannel: testutils.GetChannelID(),
+					MSTeamsID:      "mockMSTeamsID",
+				}).Return(errors.New("unable to store the link posts")).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("UpdateMessage", "mockTeamsTeamID", testutils.GetChannelID(), "", "mockMSTeamsID", "mockMessage").Return(nil).Times(1)
+				uclient.On("GetMessage", "mockTeamsTeamID", testutils.GetChannelID(), "mockMSTeamsID").Return(&msteams.Message{
+					ID: testutils.GetID(),
+				}, nil).Times(1)
+			},
+		},
+		{
+			Name: "Update: Valid",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogWarn", "Error updating the msteams/mattermost post link metadata", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockMSTeamsID",
+				}, nil).Times(1)
+				store.On("LinkPosts", storemodels.PostInfo{
+					MattermostID:   testutils.GetID(),
+					MSTeamsChannel: testutils.GetChannelID(),
+					MSTeamsID:      "mockMSTeamsID",
+				}).Return(nil).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("UpdateMessage", "mockTeamsTeamID", testutils.GetChannelID(), "", "mockMSTeamsID", "mockMessage").Return(nil).Times(1)
+				uclient.On("GetMessage", "mockTeamsTeamID", testutils.GetChannelID(), "mockMSTeamsID").Return(&msteams.Message{
+					ID: testutils.GetID(),
+				}, nil).Times(1)
+			},
+		},
+	} {
+		t.Run(test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			p := newTestPlugin()
+			test.SetupAPI(p.API.(*plugintest.API))
+			test.SetupStore(p.store.(*storemocks.Store))
+			test.SetupClient(p.msteamsAppClient.(*clientmocks.Client), p.clientBuilderWithToken("", "", nil, nil).(*clientmocks.Client))
+			mockPost := testutils.GetPost()
+			mockPost.Message = "mockMessage"
+			err := p.Update("mockTeamsTeamID", testutils.GetChannelID(), testutils.GetUser(model.ChannelAdminRoleId, "test@test.com"), mockPost, testutils.GetPost())
+			if test.ExpectedError != "" {
+				assert.Contains(err.Error(), test.ExpectedError)
+			} else {
+				assert.Nil(err)
+			}
+		})
+	}
+}
+
+func TestUpdateChat(t *testing.T) {
+	for _, test := range []struct {
+		Name          string
+		SetupAPI      func(*plugintest.API)
+		SetupStore    func(*storemocks.Store)
+		SetupClient   func(*clientmocks.Client, *clientmocks.Client)
+		ExpectedError string
+	}{
+		{
+			Name: "UpdateChat: Unable to get the post info",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogError", "Error updating post", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(nil, testutils.GetInternalServerAppError("unable to get the post info")).Times(1)
+			},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "unable to get the post info",
+		},
+		{
+			Name: "UpdateChat: Post info is nil",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogError", "Error updating post, post not found.")
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(nil, nil).Times(1)
+			},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "post not found",
+		},
+		{
+			Name:     "UpdateChat: Unable to get the client",
+			SetupAPI: func(api *plugintest.API) {},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockMSTeamsID",
+				}, nil).Times(1)
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(nil, nil).Times(1)
+				store.On("GetTokenForMattermostUser", "bot-user-id").Return(nil, nil).Times(1)
+			},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "not connected user",
+		},
+		{
+			Name: "UpdateChat: Unable to update the message",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogWarn", "Error updating the post", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockTeamsTeamID",
+				}, nil).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("UpdateChatMessage", "mockChatID", "mockTeamsTeamID", "mockMessage").Return(errors.New("unable to update the message")).Times(1)
+			},
+			ExpectedError: "unable to update the message",
+		},
+		{
+			Name: "UpdateChat: Unable to get the updated message",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogWarn", "Error updating the msteams/mattermost post link metadata", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockTeamsTeamID",
+				}, nil).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("UpdateChatMessage", "mockChatID", "mockTeamsTeamID", "mockMessage").Return(nil).Times(1)
+				uclient.On("GetChatMessage", "mockChatID", "mockTeamsTeamID").Return(nil, errors.New("unable to get the updated message")).Times(1)
+			},
+		},
+		{
+			Name: "UpdateChat: Unable to store the link posts",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogWarn", "Error updating the msteams/mattermost post link metadata", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockTeamsTeamID",
+				}, nil).Times(1)
+				store.On("LinkPosts", storemodels.PostInfo{
+					MattermostID:   testutils.GetID(),
+					MSTeamsChannel: "mockChatID",
+					MSTeamsID:      "mockTeamsTeamID",
+				}).Return(errors.New("unable to store the link posts")).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("UpdateChatMessage", "mockChatID", "mockTeamsTeamID", "mockMessage").Return(nil).Times(1)
+				uclient.On("GetChatMessage", "mockChatID", "mockTeamsTeamID").Return(&msteams.Message{
+					ID: testutils.GetID(),
+				}, nil).Times(1)
+			},
+		},
+		{
+			Name: "UpdateChat: Valid",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("LogWarn", "Error updating the msteams/mattermost post link metadata", "error", mock.Anything)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
+				store.On("GetPostInfoByMattermostID", testutils.GetID()).Return(&storemodels.PostInfo{
+					MattermostID: testutils.GetID(),
+					MSTeamsID:    "mockTeamsTeamID",
+				}, nil).Times(1)
+				store.On("LinkPosts", storemodels.PostInfo{
+					MattermostID:   testutils.GetID(),
+					MSTeamsChannel: "mockChatID",
+					MSTeamsID:      "mockTeamsTeamID",
+				}).Return(nil).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("UpdateChatMessage", "mockChatID", "mockTeamsTeamID", "mockMessage").Return(nil).Times(1)
+				uclient.On("GetChatMessage", "mockChatID", "mockTeamsTeamID").Return(&msteams.Message{
+					ID: testutils.GetID(),
+				}, nil).Times(1)
+			},
+		},
+	} {
+		t.Run(test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			p := newTestPlugin()
+			test.SetupAPI(p.API.(*plugintest.API))
+			test.SetupStore(p.store.(*storemocks.Store))
+			test.SetupClient(p.msteamsAppClient.(*clientmocks.Client), p.clientBuilderWithToken("", "", nil, nil).(*clientmocks.Client))
+			mockPost := testutils.GetPost()
+			mockPost.Message = "mockMessage"
+			err := p.UpdateChat("mockChatID", testutils.GetUser(model.ChannelAdminRoleId, "test@test.com"), mockPost, testutils.GetPost())
+			if test.ExpectedError != "" {
+				assert.Contains(err.Error(), test.ExpectedError)
+			} else {
+				assert.Nil(err)
+			}
+		})
+	}
+}
+
+func TestGetChatIDForChannel(t *testing.T) {
+	for _, test := range []struct {
+		Name           string
+		SetupAPI       func(*plugintest.API)
+		SetupStore     func(*storemocks.Store)
+		SetupClient    func(*clientmocks.Client, *clientmocks.Client)
+		ExpectedError  string
+		ExpectedResult string
+	}{
+		{
+			Name: "GetChatIDForChannel: Unable to get the channel",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("GetChannel", testutils.GetChannelID()).Return(nil, testutils.GetInternalServerAppError("unable to get the channel")).Times(1)
+			},
+			SetupStore:    func(store *storemocks.Store) {},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "unable to get the channel",
+		},
+		{
+			Name: "GetChatIDForChannel: Channel type is 'open'",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeOpen), nil).Times(1)
+			},
+			SetupStore:    func(store *storemocks.Store) {},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "invalid channel type, chatID is only available for direct messages and group messages",
+		},
+		{
+			Name: "GetChatIDForChannel: Unable to get the channel members",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeGroup), nil).Times(1)
+				api.On("GetChannelMembers", testutils.GetChannelID(), 0, 10).Return(nil, testutils.GetInternalServerAppError("unable to get the channel members")).Times(1)
+			},
+			SetupStore:    func(store *storemocks.Store) {},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "unable to get the channel members",
+		},
+		{
+			Name: "GetChatIDForChannel: Unable to store users",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeGroup), nil).Times(1)
+				api.On("GetChannelMembers", testutils.GetChannelID(), 0, 10).Return(testutils.GetChannelMembers(2), nil).Times(1)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("MattermostToTeamsUserID", testutils.GetID()).Return("", errors.New("unable to store the user")).Times(1)
+			},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "unable to store the user",
+		},
+		{
+			Name: "GetChatIDForChannel: Unable to get the client",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeGroup), nil).Times(1)
+				api.On("GetChannelMembers", testutils.GetChannelID(), 0, 10).Return(testutils.GetChannelMembers(2), nil).Times(1)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("MattermostToTeamsUserID", testutils.GetID()).Return("mockTeamsUserID", nil).Times(2)
+				store.On("GetTokenForMattermostUser", "mockClientUserID").Return(nil, nil).Times(1)
+			},
+			SetupClient:   func(client *clientmocks.Client, uclient *clientmocks.Client) {},
+			ExpectedError: "not connected user",
+		},
+		{
+			Name: "GetChatIDForChannel: Unable to create or get chat for users",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeGroup), nil).Times(1)
+				api.On("GetChannelMembers", testutils.GetChannelID(), 0, 10).Return(testutils.GetChannelMembers(2), nil).Times(1)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("MattermostToTeamsUserID", testutils.GetID()).Return("mockTeamsUserID", nil).Times(2)
+				store.On("GetTokenForMattermostUser", "mockClientUserID").Return(&oauth2.Token{}, nil).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("CreateOrGetChatForUsers", []string{"mockTeamsUserID", "mockTeamsUserID"}).Return("", errors.New("unable to create or get chat for users")).Times(1)
+			},
+			ExpectedError: "unable to create or get chat for users",
+		},
+		{
+			Name: "GetChatIDForChannel: Valid",
+			SetupAPI: func(api *plugintest.API) {
+				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeGroup), nil).Times(1)
+				api.On("GetChannelMembers", testutils.GetChannelID(), 0, 10).Return(testutils.GetChannelMembers(2), nil).Times(1)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("MattermostToTeamsUserID", testutils.GetID()).Return("mockTeamsUserID", nil).Times(2)
+				store.On("GetTokenForMattermostUser", "mockClientUserID").Return(&oauth2.Token{}, nil).Times(1)
+			},
+			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
+				uclient.On("CreateOrGetChatForUsers", []string{"mockTeamsUserID", "mockTeamsUserID"}).Return("mockChatID", nil).Times(1)
+			},
+			ExpectedResult: "mockChatID",
+		},
+	} {
+		t.Run(test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			p := newTestPlugin()
+			test.SetupAPI(p.API.(*plugintest.API))
+			test.SetupStore(p.store.(*storemocks.Store))
+			test.SetupClient(p.msteamsAppClient.(*clientmocks.Client), p.clientBuilderWithToken("", "", nil, nil).(*clientmocks.Client))
+			resp, err := p.GetChatIDForChannel("mockClientUserID", testutils.GetChannelID())
+			if test.ExpectedError != "" {
+				assert.Contains(err.Error(), test.ExpectedError)
+				assert.Equal(resp, "")
+			} else {
+				assert.Nil(err)
+				assert.Equal(resp, test.ExpectedResult)
+			}
 		})
 	}
 }
