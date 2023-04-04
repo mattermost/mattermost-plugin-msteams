28acab92775ff30a295920f93c6ee6e2a290c681
ayusht2810 <ayush.thakur@brightscout.com>
2023-04-04 12:15:13 +0530

    Add new test case and update function name

--- server/store/crypt_test.go
+++ server/store/crypt_test.go
@@ -19,7 +19,7 @@ func TestUnpad(t *testing.T) {
 	assert.Equal([]byte{0x0}, resp)
 }
 
-func TestEncrypt(t *testing.T) {
+func TestEncryptDecrypt(t *testing.T) {
 	for _, test := range []struct {
 		Name          string
 		Key           []byte
@@ -53,6 +53,75 @@ func TestEncrypt(t *testing.T) {
 			} else {
 				assert.Nil(err)
 				assert.Equal("mockData", decryptedKey)
+			}
+		})
+	}
+}
+
+func TestEncrypt(t *testing.T) {
+	for _, test := range []struct {
+		Name          string
+		Key           []byte
+		ExpectedError string
+	}{
+		{
+			Name:          "Encrypt: Invalid key",
+			Key:           make([]byte, 1),
+			ExpectedError: "could not create a cipher block",
+		},
+		{
+			Name: "Encrypt: Valid",
+			Key:  make([]byte, 16),
+		},
+	} {
+		t.Run(test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			resp, err := encrypt(test.Key, "mockData")
+			if test.ExpectedError != "" {
+				assert.Contains(err.Error(), test.ExpectedError)
+				assert.Equal("", resp)
+			} else {
+				assert.Nil(err)
+				assert.NotEqual("", resp)
+			}
+		})
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	for _, test := range []struct {
+		Name          string
+		Key           []byte
+		Text          string
+		ExpectedError string
+	}{
+		{
+			Name:          "Decrypt: Invalid key",
+			Text:          "g2E9QKddQTJn74EFipHhZ7QCW0vf1cIzXzN8xRTr9fA=",
+			Key:           make([]byte, 1),
+			ExpectedError: "could not create a cipher block",
+		},
+		{
+			Name:          "Decrypt: blocksize must be multiple of decoded message length",
+			Text:          "mockData",
+			Key:           make([]byte, 16),
+			ExpectedError: "blocksize must be multiple of decoded message length",
+		},
+		{
+			Name: "Decrypt: Valid",
+			Text: "g2E9QKddQTJn74EFipHhZ7QCW0vf1cIzXzN8xRTr9fA=",
+			Key:  make([]byte, 16),
+		},
+	} {
+		t.Run(test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			resp, err := decrypt(test.Key, test.Text)
+			if test.ExpectedError != "" {
+				assert.Contains(err.Error(), test.ExpectedError)
+				assert.Equal("", resp)
+			} else {
+				assert.Nil(err)
+				assert.NotEqual("", resp)
 			}
 		})
 	}
