c258c635d9c0a6196d652a3008d31e25e93ba1c3
Ayush Thakur <100013900+ayusht2810@users.noreply.github.com>
2023-05-17 16:57:14 +0530

    [MI-3058] Fix issue 95 of syncing mentions in the channels and chat (#111)
    * [MI-3058] Fix issue 95 of syncing mentions in that channels and chat
    
    * Move the logic of getting mentions data from client and remove plugin and store from client

--- server/handlers/converters_test.go
+++ server/handlers/converters_test.go
@@ -1,13 +1,17 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 
 	mocksPlugin "github.com/mattermost/mattermost-plugin-msteams-sync/server/handlers/mocks"
 	"github.com/mattermost/mattermost-plugin-msteams-sync/server/msteams"
+	mocksStore "github.com/mattermost/mattermost-plugin-msteams-sync/server/store/mocks"
 	"github.com/mattermost/mattermost-plugin-msteams-sync/server/testutils"
 	"github.com/mattermost/mattermost-server/v6/model"
+	"github.com/mattermost/mattermost-server/v6/plugin/plugintest"
+	"github.com/mattermost/mattermost-server/v6/plugin/plugintest/mock"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -67,6 +71,145 @@ func TestMsgToPost(t *testing.T) {
 			ah.plugin = p
 			post, _ := ah.msgToPost(testCase.userID, testCase.channelID, testCase.message, testCase.senderID)
 			assert.Equal(t, testCase.post, post)
+		})
+	}
+}
+
+func TestHandleMentions(t *testing.T) {
+	ah := ActivityHandler{}
+	for _, testCase := range []struct {
+		description     string
+		setupPlugin     func(*mocksPlugin.PluginIface, *plugintest.API, *mocksStore.Store)
+		setupAPI        func(*plugintest.API)
+		setupStore      func(*mocksStore.Store)
+		message         *msteams.Message
+		expectedMessage string
+	}{
+		{
+			description: "No mentions present",
+			setupPlugin: func(p *mocksPlugin.PluginIface, mockAPI *plugintest.API, store *mocksStore.Store) {},
+			setupAPI:    func(api *plugintest.API) {},
+			setupStore:  func(store *mocksStore.Store) {},
+			message: &msteams.Message{
+				Text: "mockMessage",
+			},
+			expectedMessage: "mockMessage",
+		},
+		{
+			description: "Channel mention present",
+			setupPlugin: func(p *mocksPlugin.PluginIface, mockAPI *plugintest.API, store *mocksStore.Store) {},
+			setupAPI:    func(api *plugintest.API) {},
+			setupStore:  func(store *mocksStore.Store) {},
+			message: &msteams.Message{
+				Text: `mockMessage <at id="0">mockMentionedText</at>`,
+				Mentions: []msteams.Mention{
+					{
+						ID:            0,
+						MentionedText: "mockMentionedText",
+					},
+				},
+			},
+			expectedMessage: "mockMessage @channel",
+		},
+		{
+			description: "Unable to get mm user ID for user mentions",
+			setupPlugin: func(p *mocksPlugin.PluginIface, mockAPI *plugintest.API, store *mocksStore.Store) {
+				p.On("GetAPI").Return(mockAPI).Once()
+				p.On("GetStore").Return(store).Once()
+			},
+			setupAPI: func(api *plugintest.API) {
+				api.On("LogDebug", "Unable to get mm UserID", "Error", mock.Anything).Once()
+			},
+			setupStore: func(store *mocksStore.Store) {
+				store.On("TeamsToMattermostUserID", testutils.GetTeamUserID()).Return("", errors.New("unable to get mm user ID"))
+			},
+			message: &msteams.Message{
+				Text: `mockMessage <at id="0">mockMentionedText</at>`,
+				Mentions: []msteams.Mention{
+					{
+						ID:            0,
+						UserID:        testutils.GetTeamUserID(),
+						MentionedText: "mockMentionedText",
+					},
+				},
+			},
+			expectedMessage: `mockMessage <at id="0">mockMentionedText</at>`,
+		},
+		{
+			description: "Unable to get mm user details for user mentions",
+			setupPlugin: func(p *mocksPlugin.PluginIface, mockAPI *plugintest.API, store *mocksStore.Store) {
+				p.On("GetAPI").Return(mockAPI).Twice()
+				p.On("GetStore").Return(store).Once()
+			},
+			setupAPI: func(api *plugintest.API) {
+				api.On("LogDebug", "Unable to get mm user details", "Error", mock.Anything).Once()
+				api.On("GetUser", testutils.GetMattermostID()).Return(nil, testutils.GetInternalServerAppError("unable to get mm user details")).Once()
+			},
+			setupStore: func(store *mocksStore.Store) {
+				store.On("TeamsToMattermostUserID", testutils.GetTeamUserID()).Return(testutils.GetMattermostID(), nil).Once()
+			},
+			message: &msteams.Message{
+				Text: `mockMessage <at id="0">mockMentionedText</at>`,
+				Mentions: []msteams.Mention{
+					{
+						ID:            0,
+						UserID:        testutils.GetTeamUserID(),
+						MentionedText: "mockMentionedText",
+					},
+				},
+			},
+			expectedMessage: `mockMessage <at id="0">mockMentionedText</at>`,
+		},
+		{
+			description: "Successful user mentions",
+			setupPlugin: func(p *mocksPlugin.PluginIface, mockAPI *plugintest.API, store *mocksStore.Store) {
+				p.On("GetAPI").Return(mockAPI).Twice()
+				p.On("GetStore").Return(store).Twice()
+			},
+			setupAPI: func(api *plugintest.API) {
+				api.On("GetUser", "mockMMUserID-1").Return(&model.User{
+					Id:       "mockMMUserID-1",
+					Username: "mockMMUsername-1",
+				}, nil).Once()
+				api.On("GetUser", "mockMMUserID-2").Return(&model.User{
+					Id:       "mockMMUserID-2",
+					Username: "mockMMUsername-2",
+				}, nil).Once()
+			},
+			setupStore: func(store *mocksStore.Store) {
+				store.On("TeamsToMattermostUserID", "mockMSUserID-1").Return("mockMMUserID-1", nil).Once()
+				store.On("TeamsToMattermostUserID", "mockMSUserID-2").Return("mockMMUserID-2", nil).Once()
+			},
+			message: &msteams.Message{
+				Text: `hello <at id="0">mockMSUsername-1</at> from <at id="1">mockMSUsername-2</at>`,
+				Mentions: []msteams.Mention{
+					{
+						ID:            0,
+						UserID:        "mockMSUserID-1",
+						MentionedText: "mockMSUsername-1",
+					},
+					{
+						ID:            1,
+						UserID:        "mockMSUserID-2",
+						MentionedText: "mockMSUsername-2",
+					},
+				},
+			},
+			expectedMessage: "hello @mockMMUsername-1  from @mockMMUsername-2 ",
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+			assert := assert.New(t)
+			p := mocksPlugin.NewPluginIface(t)
+			store := mocksStore.NewStore(t)
+			mockAPI := &plugintest.API{}
+			testCase.setupPlugin(p, mockAPI, store)
+			testCase.setupAPI(mockAPI)
+			testCase.setupStore(store)
+
+			ah.plugin = p
+			message := ah.handleMentions(testCase.message)
+			assert.Equal(testCase.expectedMessage, message)
 		})
 	}
 }
--- server/msteams/client.go
+++ server/msteams/client.go
@@ -17,7 +17,6 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/enescakir/emoji"
 	msgraphsdk "github.com/microsoftgraph/msgraph-beta-sdk-go"
 	"github.com/microsoftgraph/msgraph-beta-sdk-go/chats"
 	"github.com/microsoftgraph/msgraph-beta-sdk-go/drives"
@@ -26,7 +25,6 @@ import (
 	"github.com/microsoftgraph/msgraph-beta-sdk-go/teams"
 	"github.com/microsoftgraph/msgraph-beta-sdk-go/users"
 	a "github.com/microsoftgraph/msgraph-sdk-go-core/authentication"
-	"gitlab.com/golang-commonmark/markdown"
 	"golang.org/x/oauth2"
 )
 
@@ -95,6 +93,12 @@ type Reaction struct {
 	Reaction string
 }
 
+type Mention struct {
+	ID            int32
+	UserID        string
+	MentionedText string
+}
+
 type Message struct {
 	ID              string
 	UserID          string
@@ -104,6 +108,7 @@ type Message struct {
 	ReplyToID       string
 	Attachments     []Attachment
 	Reactions       []Reaction
+	Mentions        []Mention
 	ChannelID       string
 	TeamID          string
 	ChatID          string
@@ -277,13 +282,11 @@ func (tc *ClientImpl) GetMe() (*User, error) {
 }
 
 func (tc *ClientImpl) SendMessage(teamID, channelID, parentID, message string) (*Message, error) {
-	return tc.SendMessageWithAttachments(teamID, channelID, parentID, message, nil)
+	return tc.SendMessageWithAttachments(teamID, channelID, parentID, message, nil, nil)
 }
 
-func (tc *ClientImpl) SendMessageWithAttachments(teamID, channelID, parentID, message string, attachments []*Attachment) (*Message, error) {
+func (tc *ClientImpl) SendMessageWithAttachments(teamID, channelID, parentID, message string, attachments []*Attachment, mentions []models.ChatMessageMentionable) (*Message, error) {
 	rmsg := models.NewChatMessage()
-	md := markdown.New(markdown.XHTMLOutput(true))
-	content := md.RenderToString([]byte(emoji.Parse(message)))
 
 	msteamsAttachments := []models.ChatMessageAttachmentable{}
 	for _, a := range attachments {
@@ -295,15 +298,16 @@ func (tc *ClientImpl) SendMessageWithAttachments(teamID, channelID, parentID, me
 		attachment.SetContentUrl(&att.ContentURL)
 		attachment.SetName(&att.Name)
 		msteamsAttachments = append(msteamsAttachments, attachment)
-		content = "<attachment id=\"" + att.ID + "\"></attachment>" + content
+		message = "<attachment id=\"" + att.ID + "\"></attachment>" + message
 	}
 	rmsg.SetAttachments(msteamsAttachments)
+	rmsg.SetMentions(mentions)
 
 	contentType := models.HTML_BODYTYPE
 
 	body := models.NewItemBody()
 	body.SetContentType(&contentType)
-	body.SetContent(&content)
+	body.SetContent(&message)
 	rmsg.SetBody(body)
 
 	var res models.ChatMessageable
@@ -323,10 +327,8 @@ func (tc *ClientImpl) SendMessageWithAttachments(teamID, channelID, parentID, me
 	return convertToMessage(res, teamID, channelID, ""), nil
 }
 
-func (tc *ClientImpl) SendChat(chatID, parentID, message string) (*Message, error) {
+func (tc *ClientImpl) SendChat(chatID, parentID, message string, mentions []models.ChatMessageMentionable) (*Message, error) {
 	rmsg := models.NewChatMessage()
-	md := markdown.New(markdown.XHTMLOutput(true))
-	content := md.RenderToString([]byte(emoji.Parse(message)))
 
 	// TODO: Add support for parent id
 	_ = parentID
@@ -335,8 +337,10 @@ func (tc *ClientImpl) SendChat(chatID, parentID, message string) (*Message, erro
 
 	body := models.NewItemBody()
 	body.SetContentType(&contentType)
-	body.SetContent(&content)
+	body.SetContent(&message)
 	rmsg.SetBody(body)
+
+	rmsg.SetMentions(mentions)
 
 	res, err := tc.client.ChatsById(chatID).Messages().Post(tc.ctx, rmsg, nil)
 	if err != nil {
@@ -410,16 +414,15 @@ func (tc *ClientImpl) DeleteChatMessage(chatID, msgID string) error {
 	return tc.client.ChatsById(chatID).MessagesById(msgID).Delete(tc.ctx, nil)
 }
 
-func (tc *ClientImpl) UpdateMessage(teamID, channelID, parentID, msgID, message string) error {
+func (tc *ClientImpl) UpdateMessage(teamID, channelID, parentID, msgID, message string, mentions []models.ChatMessageMentionable) error {
 	rmsg := models.NewChatMessage()
-	md := markdown.New(markdown.XHTMLOutput(true), markdown.LangPrefix("CodeMirror language-"))
-	content := md.RenderToString([]byte(emoji.Parse(message)))
 
 	contentType := models.HTML_BODYTYPE
+	rmsg.SetMentions(mentions)
 
 	body := models.NewItemBody()
 	body.SetContentType(&contentType)
-	body.SetContent(&content)
+	body.SetContent(&message)
 	rmsg.SetBody(body)
 
 	if parentID != "" {
@@ -434,16 +437,16 @@ func (tc *ClientImpl) UpdateMessage(teamID, channelID, parentID, msgID, message 
 	return nil
 }
 
-func (tc *ClientImpl) UpdateChatMessage(chatID, msgID, message string) error {
+func (tc *ClientImpl) UpdateChatMessage(chatID, msgID, message string, mentions []models.ChatMessageMentionable) error {
 	rmsg := models.NewChatMessage()
-	md := markdown.New(markdown.XHTMLOutput(true), markdown.LangPrefix("CodeMirror language-"))
-	content := md.RenderToString([]byte(emoji.Parse(message)))
 
 	contentType := models.HTML_BODYTYPE
 
+	rmsg.SetMentions(mentions)
+
 	body := models.NewItemBody()
 	body.SetContentType(&contentType)
-	body.SetContent(&content)
+	body.SetContent(&message)
 	rmsg.SetBody(body)
 
 	if _, err := tc.client.ChatsById(chatID).MessagesById(msgID).Patch(tc.ctx, rmsg, nil); err != nil {
@@ -704,6 +707,23 @@ func convertToMessage(msg models.ChatMessageable, teamID, channelID, chatID stri
 		})
 	}
 
+	mentions := []Mention{}
+	for _, m := range msg.GetMentions() {
+		mention := Mention{}
+		if m.GetId() != nil && m.GetMentionText() != nil {
+			mention.ID = *m.GetId()
+			mention.MentionedText = *m.GetMentionText()
+		} else {
+			continue
+		}
+
+		if m.GetMentioned() != nil && m.GetMentioned().GetUser() != nil && m.GetMentioned().GetUser().GetId() != nil {
+			mention.UserID = *m.GetMentioned().GetUser().GetId()
+		}
+
+		mentions = append(mentions, mention)
+	}
+
 	reactions := []Reaction{}
 	for _, reaction := range msg.GetReactions() {
 		if reaction.GetReactionType() != nil && reaction.GetUser() != nil && reaction.GetUser().GetUser() != nil && reaction.GetUser().GetUser().GetId() != nil {
@@ -719,6 +739,7 @@ func convertToMessage(msg models.ChatMessageable, teamID, channelID, chatID stri
 		ReplyToID:       replyTo,
 		Subject:         subject,
 		Attachments:     attachments,
+		Mentions:        mentions,
 		TeamID:          teamID,
 		ChannelID:       channelID,
 		ChatID:          chatID,
