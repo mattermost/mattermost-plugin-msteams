c7de66ace3d2ba3c47ee5e8898778312b0eaf1c3
Doug Lauder <wiggin77@warpmail.net>
2023-07-09 16:54:24 -0400

    Minor update to iframe support (#207)
    * set samesite for MMEMBED cookie

--- server/iframe.go
+++ server/iframe.go
@@ -38,6 +38,16 @@ func (a *API) iFrame(w http.ResponseWriter, _ *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html")
 
+	// set session cookie to indicate Mattermost is hosted in an iFrame, which allows
+	// webapp to bypass "Where do you want to view this" page and set SameSite=none.
+	http.SetCookie(w, &http.Cookie{
+		Name:     "MMEMBED",
+		Value:    "1",
+		Path:     "/",
+		Secure:   true,
+		SameSite: http.SameSiteNoneMode,
+	})
+
 	if _, err := w.Write([]byte(html)); err != nil {
 		a.p.API.LogError("Unable to serve the iFrame", "Error", err.Error())
 	}
