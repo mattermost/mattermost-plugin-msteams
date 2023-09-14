4fd30cf2366e90ed64529ec682dd171acef21a9c
Jesús Espino <jespinog@gmail.com>
2023-09-14 19:16:20 +0200

    Fixing CI

--- server/handlers/converters_test.go
+++ server/handlers/converters_test.go
@@ -47,7 +47,6 @@ func TestMsgToPost(t *testing.T) {
 				p.On("GetBotUserID").Return(testutils.GetSenderID())
 				p.On("GetURL").Return("https://example.com/")
 				p.On("GetClientForApp").Return(client)
-				p.On("GetAPI").Return(mockAPI).Once()
 			},
 			setupAPI: func(api *plugintest.API) {
 				api.On("LogDebug", "Unable to get user avatar", "Error", mock.Anything).Once()
