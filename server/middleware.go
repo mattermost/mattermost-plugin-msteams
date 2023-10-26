c4e97931d6878bddcb4a0672adf33598287527a1
Jesse Hallam <jesse.hallam@gmail.com>
2023-10-26 16:56:13 -0300

    skip != nil in middleware, since redundant

--- server/middleware.go
+++ server/middleware.go
@@ -20,34 +20,30 @@ func (r *StatusRecorder) WriteHeader(status int) {
 
 func (a *API) metricsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if a.p.GetMetrics() != nil {
-			a.p.GetMetrics().IncrementHTTPRequests()
-			recorder := &StatusRecorder{
-				ResponseWriter: w,
-				Status:         http.StatusOK,
-			}
-
-			now := time.Now()
-			next.ServeHTTP(recorder, r)
-			elapsed := float64(time.Since(now)) / float64(time.Second)
-
-			if recorder.Status < 200 || recorder.Status > 299 {
-				a.p.GetMetrics().IncrementHTTPErrors()
-			}
-
-			var routeMatch mux.RouteMatch
-			a.router.Match(r, &routeMatch)
-			endpoint := "unknown"
-			if routeMatch.Route != nil {
-				var err error
-				endpoint, err = routeMatch.Route.GetPathTemplate()
-				if err != nil {
-					endpoint = "unknown"
-				}
-			}
-			a.p.GetMetrics().ObserveAPIEndpointDuration(endpoint, r.Method, strconv.Itoa(recorder.Status), elapsed)
-		} else {
-			next.ServeHTTP(w, r)
+		a.p.GetMetrics().IncrementHTTPRequests()
+		recorder := &StatusRecorder{
+			ResponseWriter: w,
+			Status:         http.StatusOK,
 		}
+
+		now := time.Now()
+		next.ServeHTTP(recorder, r)
+		elapsed := float64(time.Since(now)) / float64(time.Second)
+
+		if recorder.Status < 200 || recorder.Status > 299 {
+			a.p.GetMetrics().IncrementHTTPErrors()
+		}
+
+		var routeMatch mux.RouteMatch
+		a.router.Match(r, &routeMatch)
+		endpoint := "unknown"
+		if routeMatch.Route != nil {
+			var err error
+			endpoint, err = routeMatch.Route.GetPathTemplate()
+			if err != nil {
+				endpoint = "unknown"
+			}
+		}
+		a.p.GetMetrics().ObserveAPIEndpointDuration(endpoint, r.Method, strconv.Itoa(recorder.Status), elapsed)
 	})
 }
