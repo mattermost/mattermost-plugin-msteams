Brightscout/mattermost-plugin-msteams-sync

[MI-3292] Added API to get MS Teams team channels (#18)
* [MI-3284] Created API to disconnect a user account

* [MI-3284] Adding testcases for disconnect API

* [MI-3284] Self review fixes

* [MI-3284] Removed usused code

* [MI-3284] Review fixes

* [MI-3287] Added API to get connected channels

* [MI-3287] Added check to skip the channels for which user does not have permissions

* [MI-3287] Added some additional logs and fixed lint errors

* [MI-3287] Fixed testcases

* [MI-3287] Added testcases for getConnectedChannels

* [MI-3287] Removed unused code

* [MI-3287] Review fixes and some code changes.
1. Added channel type in response.
2. Added condition if no links are present.

* [MI-3284] Added middleware for authorization and some testcases refactoring

* [MI-3287] Added check for authorization and some testcases refactoring

* [MI-3284] Review fixes
1. Created middleware for checking if user is connected of not.
2. Created sub routers for auto-complete APIs

* [MI-3287] Review fixes

* Bump plugin version

* [MI-3291] Created API to get the list of MS Teams teams.

* [MI-3291] Added pagination and added testcases for getMSTeamsTeamList

* [MI-3291] Used common function for writing JSON response

* [MI-3291] Testcases refactoring

* [MI-3291] Added search query in getMSTeamsTeams API

* [MI-3291] Review fixes

* [MI-3287] Review Fixes

* [MI-3284] Removed OPTIONS http method from APIs

* [MI-3291] Refactored pagination code

* [MI-3291] Refactored search query

* [MI-3291] Used common function for writing JSON response

* [MI-3292] Added API to get MS Teams team channels

* [MI-3292] Code refactoring

* [MI-3292] Added testcases for getMSTeamsTeamChannels

* [MI-3292] Added middleware for authorization

* [MI-3292] Reduced code repetition

* [MI-3292] Self review fix

* [MI-3292] Added search query

* [MI-3292] FIx testcases

* [MI-3291] Review fixes

* [MI-3292] Review fixes

* [M

server/api.go
@@ -19,7 +19,12 @@ import (
 
 const (
 	HeaderMattermostUserID = "Mattermost-User-Id"
-	QueryParamSearchTerm   = "search"
+
+	// Query params
+	QueryParamSearchTerm = "search"
+
+	// Path params
+	PathParamTeamID = "team_id"
 
 	// Used for storing the token in the request context to pass from one middleware to another
 	// #nosec G101 -- This is a false positive. The below line is not a hardcoded credential
@@ -61,6 +66,7 @@ func NewAPI(p *Plugin, store store.Store) *API {
 
 	// MS Teams APIs
 	msTeamsRouter.HandleFunc("/teams", api.handleAuthRequired(api.checkUserConnected(api.getMSTeamsTeamList))).Methods(http.MethodGet)
+	msTeamsRouter.HandleFunc(fmt.Sprintf("/teams/{%s:[A-Za-z0-9-]{36}}/channels", PathParamTeamID), api.handleAuthRequired(api.checkUserConnected(api.getMSTeamsTeamChannels))).Methods(http.MethodGet)
 
 	// Command autocomplete APIs
 	autocompleteRouter.HandleFunc("/teams", api.autocompleteTeams).Methods(http.MethodGet)
@@ -209,18 +215,9 @@ func (a *API) autocompleteChannels(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	client, err := a.p.GetClientForUser(userID)
-	if err != nil {
-		a.p.API.LogError("Unable to get the client for user", "MMUserID", userID, "Error", err.Error())
-		data, _ := json.Marshal(out)
-		_, _ = w.Write(data)
-		return
-	}
-
 	teamID := args[2]
-	channels, err := client.ListChannels(teamID)
+	channels, _, err := a.p.GetMSTeamsTeamChannels(teamID, userID, r)
 	if err != nil {
-		a.p.API.LogError("Unable to get the channels for MS Teams team", "TeamID", teamID, "Error", err.Error())
 		data, _ := json.Marshal(out)
 		_, _ = w.Write(data)
 		return
@@ -401,6 +398,41 @@ func (a *API) getMSTeamsTeamList(w http.ResponseWriter, r *http.Request) {
 	}
 
 	a.writeJSONArray(w, http.StatusOK, paginatedTeams)
+}
+
+func (a *API) getMSTeamsTeamChannels(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	teamID := pathParams[PathParamTeamID]
+	userID := r.Header.Get(HeaderMattermostUserID)
+	channels, statusCode, err := a.p.GetMSTeamsTeamChannels(teamID, userID, r)
+	if err != nil {
+		http.Error(w, "Error occurred while fetching the MS Teams team channels.", statusCode)
+		return
+	}
+
+	sort.Slice(channels, func(i, j int) bool {
+		return fmt.Sprintf("%s_%s", channels[i].DisplayName, channels[i].ID) < fmt.Sprintf("%s_%s", channels[j].DisplayName, channels[j].ID)
+	})
+
+	searchTerm := r.URL.Query().Get(QueryParamSearchTerm)
+	offset, limit := a.p.GetOffsetAndLimit(r.URL.Query())
+	paginatedChannels := []*msteams.Channel{}
+	matchCount := 0
+	for _, channel := range channels {
+		if len(paginatedChannels) == limit {
+			break
+		}
+
+		if strings.HasPrefix(strings.ToLower(channel.DisplayName), strings.ToLower(searchTerm)) {
+			if matchCount >= offset {
+				paginatedChannels = append(paginatedChannels, channel)
+			} else {
+				matchCount++
+			}
+		}
+	}
+
+	a.writeJSONArray(w, http.StatusOK, paginatedChannels)
 }
 
 // TODO: Add unit tests

server/api_test.go
@@ -617,7 +617,7 @@ func TestAutocompleteChannels(t *testing.T) {
 				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
 			},
 			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
-				uclient.On("ListChannels", "mockData-3").Return([]msteams.Channel{
+				uclient.On("ListChannels", "mockData-3").Return([]*msteams.Channel{
 					{
 						ID:          "mockTeamsTeamID-1",
 						DisplayName: "mockDisplayName-1",
@@ -922,7 +922,7 @@ func TestDisconnect(t *testing.T) {
 	}
 }
 
-func TestGetConnectedChannels(t *testing.T) {
+func TestGetLinkedChannels(t *testing.T) {
 	for _, test := range []struct {
 		Name               string
 		SetupPlugin        func(*plugintest.API)
@@ -932,7 +932,7 @@ func TestGetConnectedChannels(t *testing.T) {
 		ExpectedStatusCode int
 	}{
 		{
-			Name: "GetConnectedChannels: linked channels listed successfully",
+			Name: "TestGetLinkedChannels: linked channels listed successfully",
 			SetupPlugin: func(api *plugintest.API) {
 				api.On("HasPermissionToChannel", testutils.GetUserID(), testutils.GetChannelID(), model.PermissionCreatePost).Return(true).Times(1)
 				api.On("GetChannel", testutils.GetChannelID()).Return(&model.Channel{}, nil).Times(1)
@@ -960,7 +960,7 @@ func TestGetConnectedChannels(t *testing.T) {
 			ExpectedStatusCode: http.StatusOK,
 		},
 		{
-			Name: "GetConnectedChannels: error occurred while getting the linked channels",
+			Name: "TestGetLinkedChannels: error occurred while getting the linked channels",
 			SetupPlugin: func(api *plugintest.API) {
 				api.On("LogError", "Error occurred while getting the linked channels", "Error", "error occurred while getting the linked channels").Times(1)
 			},
@@ -972,7 +972,7 @@ func TestGetConnectedChannels(t *testing.T) {
 			ExpectedStatusCode: http.StatusInternalServerError,
 		},
 		{
-			Name: "GetConnectedChannels: error occurred while getting the MS Teams teams details",
+			Name: "TestGetLinkedChannels: error occurred while getting the MS Teams teams details",
 			SetupPlugin: func(api *plugintest.API) {
 				api.On("HasPermissionToChannel", testutils.GetUserID(), testutils.GetChannelID(), model.PermissionCreatePost).Return(true).Times(1)
 				api.On("LogDebug", "Unable to get the MS Teams teams information", "Error", "error occurred while getting the MS Teams teams details")
@@ -990,7 +990,7 @@ func TestGetConnectedChannels(t *testing.T) {
 			ExpectedStatusCode: http.StatusInternalServerError,
 		},
 		{
-			Name: "GetConnectedChannels: error occurred while getting channel details",
+			Name: "TestGetLinkedChannels: error occurred while getting channel details",
 			SetupPlugin: func(api *plugintest.API) {
 				api.On("HasPermissionToChannel", testutils.GetUserID(), testutils.GetChannelID(), model.PermissionCreatePost).Return(true).Times(1)
 				api.On("GetChannel", testutils.GetChannelID()).Return(nil, &model.AppError{
@@ -1120,6 +1120,95 @@ func TestGetMSTeamsTeamList(t *testing.T) {
 
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodGet, "/msteams/teams", nil)
+			r.Header.Add(HeaderMattermostUserID, testutils.GetUserID())
+			queryParams := url.Values{
+				QueryParamPerPage: {fmt.Sprint(DefaultPerPageLimit)},
+				QueryParamPage:    {fmt.Sprint(DefaultPage)},
+			}
+
+			r.URL.RawQuery = queryParams.Encode()
+			plugin.ServeHTTP(nil, w, r)
+			result := w.Result()
+			assert.NotNil(t, result)
+			defer result.Body.Close()
+
+			bodyBytes, err := io.ReadAll(result.Body)
+			assert.Nil(err)
+
+			bodyString := string(bodyBytes)
+			assert.Equal(test.ExpectedResult, bodyString)
+			assert.Equal(result.StatusCode, test.ExpectedStatusCode)
+		})
+	}
+}
+
+func TestGetMSTeamsTeamChannels(t *testing.T) {
+	for _, test := range []struct {
+		Name               string
+		SetupPlugin        func(*plugintest.API)
+		SetupStore         func(*storemocks.Store)
+		SetupClient        func(*clientmocks.Client)
+		QueryParamTeamID   string
+		ExpectedResult     string
+		ExpectedStatusCode int
+	}{
+		{
+			Name: "TestGetMSTeamsTeamChannels: MS Teams team channels listed successfully",
+			SetupPlugin: func(api *plugintest.API) {
+				api.On("GetConfig").Return(&model.Config{ServiceSettings: model.ServiceSettings{SiteURL: model.NewString("/")}}, nil).Times(1)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetUserID()).Return(&oauth2.Token{}, nil).Times(1)
+			},
+			SetupClient: func(c *clientmocks.Client) {
+				c.On("ListChannels", testutils.GetTeamsTeamID()).Return([]*msteams.Channel{
+					{
+						ID:          "mockTeamsChannelID-1",
+						DisplayName: "mockDisplayName-1",
+						Description: "mockDescription-1",
+					},
+					{
+						ID:          "mockTeamsChannelID-2",
+						DisplayName: "mockDisplayName-2",
+						Description: "mockDescription-2",
+					},
+				}, nil).Times(1)
+			},
+			QueryParamTeamID:   testutils.GetTeamsTeamID(),
+			ExpectedResult:     `[{"ID":"mockTeamsChannelID-1","DisplayName":"mockDisplayName-1","Description":"mockDescription-1"},{"ID":"mockTeamsChannelID-2","DisplayName":"mockDisplayName-2","Description":"mockDescription-2"}]`,
+			ExpectedStatusCode: http.StatusOK,
+		},
+		{
+			Name: "TestGetMSTeamsTeamChannels: error occurred while getting MS Teams team channels",
+			SetupPlugin: func(api *plugintest.API) {
+				api.On("GetConfig").Return(&model.Config{ServiceSettings: model.ServiceSettings{SiteURL: model.NewString("/")}}, nil).Times(1)
+				api.On("LogError", "Unable to get the channels for MS Teams team", "TeamID", testutils.GetTeamsTeamID(), "Error", "error occurred while getting MS Teams team channels").Times(1)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetUserID()).Return(&oauth2.Token{}, nil).Times(1)
+			},
+			SetupClient: func(c *clientmocks.Client) {
+				c.On("ListChannels", testutils.GetTeamsTeamID()).Return(nil, errors.New("error occurred while getting MS Teams team channels")).Times(1)
+			},
+			QueryParamTeamID:   testutils.GetTeamsTeamID(),
+			ExpectedResult:     "Error occurred while fetching the MS Teams team channels.\n",
+			ExpectedStatusCode: http.StatusInternalServerError,
+		},
+	} {
+		t.Run(test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			plugin := newTestPlugin(t)
+			mockAPI := &plugintest.API{}
+
+			plugin.SetAPI(mockAPI)
+			defer mockAPI.AssertExpectations(t)
+
+			test.SetupPlugin(mockAPI)
+			test.SetupStore(plugin.store.(*storemocks.Store))
+			test.SetupClient(plugin.clientBuilderWithToken("", "", "", "", nil, nil).(*clientmocks.Client))
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/msteams/teams/%s/channels", testutils.GetTeamsTeamID()), nil)
 			r.Header.Add(HeaderMattermostUserID, testutils.GetUserID())
 			queryParams := url.Values{
 				QueryParamPerPage: {fmt.Sprint(DefaultPerPageLimit)},

server/get_teams_data.go
@@ -117,6 +117,28 @@ func (p *Plugin) GetMSTeamsTeamList(userID string, r *http.Request) ([]*msteams.
 	return teams, http.StatusOK, nil
 }
 
+func (p *Plugin) GetMSTeamsTeamChannels(teamID, userID string, r *http.Request) ([]*msteams.Channel, int, error) {
+	var client msteams.Client
+	var err error
+	if r.Context().Value(ContextClientKey) == nil {
+		client, err = p.GetClientForUser(userID)
+		if err != nil {
+			p.API.LogError("Unable to get the client for user", "MMUserID", userID, "Error", err.Error())
+			return nil, http.StatusUnauthorized, err
+		}
+	} else {
+		client = r.Context().Value(ContextClientKey).(msteams.Client)
+	}
+
+	channels, err := client.ListChannels(teamID)
+	if err != nil {
+		p.API.LogError("Unable to get the channels for MS Teams team", "TeamID", teamID, "Error", err.Error())
+		return nil, http.StatusInternalServerError, err
+	}
+
+	return channels, http.StatusOK, nil
+}
+
 func (p *Plugin) GetOffsetAndLimit(query url.Values) (offset, limit int) {
 	var page int
 	if val, err := strconv.Atoi(query.Get(QueryParamPage)); err != nil || val < 0 {

server/msteams/mocks/Client.go
@@ -472,15 +472,15 @@ func (_m *Client) GetUserAvatar(userID string) ([]byte, error) {
 }
 
 // ListChannels provides a mock function with given fields: teamID
-func (_m *Client) ListChannels(teamID string) ([]msteams.Channel, error) {
+func (_m *Client) ListChannels(teamID string) ([]*msteams.Channel, error) {
 	ret := _m.Called(teamID)
 
-	var r0 []msteams.Channel
-	if rf, ok := ret.Get(0).(func(string) []msteams.Channel); ok {
+	var r0 []*msteams.Channel
+	if rf, ok := ret.Get(0).(func(string) []*msteams.Channel); ok {
 		r0 = rf(teamID)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).([]msteams.Channel)
+			r0 = ret.Get(0).([]*msteams.Channel)
 		}
 	}
 

mattermost/mattermost-plugin-msteams

server/api.go
@@ -23,7 +23,12 @@ import (
 
 const (
 	HeaderMattermostUserID = "Mattermost-User-Id"
-	QueryParamSearchTerm   = "search"
+
+	// Query params
+	QueryParamSearchTerm = "search"
+
+	// Path params
+	PathParamTeamID = "team_id"
 
 	// Used for storing the token in the request context to pass from one middleware to another
 	// #nosec G101 -- This is a false positive. The below line is not a hardcoded credential
@@ -78,6 +83,7 @@ func NewAPI(p *Plugin, store store.Store) *API {
 
 	// MS Teams APIs
 	msTeamsRouter.HandleFunc("/teams", api.handleAuthRequired(api.checkUserConnected(api.getMSTeamsTeamList))).Methods(http.MethodGet)
+	msTeamsRouter.HandleFunc(fmt.Sprintf("/teams/{%s:[A-Za-z0-9-]{36}}/channels", PathParamTeamID), api.handleAuthRequired(api.checkUserConnected(api.getMSTeamsTeamChannels))).Methods(http.MethodGet)
 
 	// Command autocomplete APIs
 	autocompleteRouter.HandleFunc("/teams", api.autocompleteTeams).Methods(http.MethodGet)
@@ -226,18 +232,9 @@ func (a *API) autocompleteChannels(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	client, err := a.p.GetClientForUser(userID)
-	if err != nil {
-		a.p.API.LogError("Unable to get the client for user", "MMUserID", userID, "Error", err.Error())
-		data, _ := json.Marshal(out)
-		_, _ = w.Write(data)
-		return
-	}
-
 	teamID := args[2]
-	channels, err := client.ListChannels(teamID)
+	channels, _, err := a.p.GetMSTeamsTeamChannels(teamID, userID, r)
 	if err != nil {
-		a.p.API.LogError("Unable to get the channels for MS Teams team", "TeamID", teamID, "Error", err.Error())
 		data, _ := json.Marshal(out)
 		_, _ = w.Write(data)
 		return
@@ -418,6 +415,41 @@ func (a *API) getMSTeamsTeamList(w http.ResponseWriter, r *http.Request) {
 	}
 
 	a.writeJSONArray(w, http.StatusOK, paginatedTeams)
+}
+
+func (a *API) getMSTeamsTeamChannels(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	teamID := pathParams[PathParamTeamID]
+	userID := r.Header.Get(HeaderMattermostUserID)
+	channels, statusCode, err := a.p.GetMSTeamsTeamChannels(teamID, userID, r)
+	if err != nil {
+		http.Error(w, "Error occurred while fetching the MS Teams team channels.", statusCode)
+		return
+	}
+
+	sort.Slice(channels, func(i, j int) bool {
+		return fmt.Sprintf("%s_%s", channels[i].DisplayName, channels[i].ID) < fmt.Sprintf("%s_%s", channels[j].DisplayName, channels[j].ID)
+	})
+
+	searchTerm := r.URL.Query().Get(QueryParamSearchTerm)
+	offset, limit := a.p.GetOffsetAndLimit(r.URL.Query())
+	paginatedChannels := []*clientmodels.Channel{}
+	matchCount := 0
+	for _, channel := range channels {
+		if len(paginatedChannels) == limit {
+			break
+		}
+
+		if strings.HasPrefix(strings.ToLower(channel.DisplayName), strings.ToLower(searchTerm)) {
+			if matchCount >= offset {
+				paginatedChannels = append(paginatedChannels, channel)
+			} else {
+				matchCount++
+			}
+		}
+	}
+
+	a.writeJSONArray(w, http.StatusOK, paginatedChannels)
 }
 
 // TODO: Add unit tests

server/api_test.go
@@ -675,7 +675,7 @@ func TestAutocompleteChannels(t *testing.T) {
 				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&oauth2.Token{}, nil).Times(1)
 			},
 			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
-				uclient.On("ListChannels", "mockData-3").Return([]clientmodels.Channel{
+				uclient.On("ListChannels", "mockData-3").Return([]*clientmodels.Channel{
 					{
 						ID:          "mockTeamsTeamID-1",
 						DisplayName: "mockDisplayName-1",
@@ -1174,7 +1174,7 @@ func TestDisconnect(t *testing.T) {
 	}
 }
 
-func TestGetConnectedChannels(t *testing.T) {
+func TestGetLinkedChannels(t *testing.T) {
 	for _, test := range []struct {
 		Name               string
 		SetupPlugin        func(*plugintest.API)
@@ -1184,7 +1184,7 @@ func TestGetConnectedChannels(t *testing.T) {
 		ExpectedStatusCode int
 	}{
 		{
-			Name: "GetConnectedChannels: linked channels listed successfully",
+			Name: "TestGetLinkedChannels: linked channels listed successfully",
 			SetupPlugin: func(api *plugintest.API) {
 				api.On("HasPermissionToChannel", testutils.GetUserID(), testutils.GetChannelID(), model.PermissionCreatePost).Return(true).Times(1)
 				api.On("GetChannel", testutils.GetChannelID()).Return(&model.Channel{}, nil).Times(1)
@@ -1212,7 +1212,7 @@ func TestGetConnectedChannels(t *testing.T) {
 			ExpectedStatusCode: http.StatusOK,
 		},
 		{
-			Name: "GetConnectedChannels: error occurred while getting the linked channels",
+			Name: "TestGetLinkedChannels: error occurred while getting the linked channels",
 			SetupPlugin: func(api *plugintest.API) {
 				api.On("LogError", "Error occurred while getting the linked channels", "Error", "error occurred while getting the linked channels").Times(1)
 			},
@@ -1224,7 +1224,7 @@ func TestGetConnectedChannels(t *testing.T) {
 			ExpectedStatusCode: http.StatusInternalServerError,
 		},
 		{
-			Name: "GetConnectedChannels: error occurred while getting the MS Teams teams details",
+			Name: "TestGetLinkedChannels: error occurred while getting the MS Teams teams details",
 			SetupPlugin: func(api *plugintest.API) {
 				api.On("HasPermissionToChannel", testutils.GetUserID(), testutils.GetChannelID(), model.PermissionCreatePost).Return(true).Times(1)
 				api.On("LogDebug", "Unable to get the MS Teams teams information", "Error", "error occurred while getting the MS Teams teams details")
@@ -1242,7 +1242,7 @@ func TestGetConnectedChannels(t *testing.T) {
 			ExpectedStatusCode: http.StatusInternalServerError,
 		},
 		{
-			Name: "GetConnectedChannels: error occurred while getting channel details",
+			Name: "TestGetLinkedChannels: error occurred while getting channel details",
 			SetupPlugin: func(api *plugintest.API) {
 				api.On("HasPermissionToChannel", testutils.GetUserID(), testutils.GetChannelID(), model.PermissionCreatePost).Return(true).Times(1)
 				api.On("GetChannel", testutils.GetChannelID()).Return(nil, &model.AppError{
@@ -1372,6 +1372,95 @@ func TestGetMSTeamsTeamList(t *testing.T) {
 
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodGet, "/msteams/teams", nil)
+			r.Header.Add(HeaderMattermostUserID, testutils.GetUserID())
+			queryParams := url.Values{
+				QueryParamPerPage: {fmt.Sprint(DefaultPerPageLimit)},
+				QueryParamPage:    {fmt.Sprint(DefaultPage)},
+			}
+
+			r.URL.RawQuery = queryParams.Encode()
+			plugin.ServeHTTP(nil, w, r)
+			result := w.Result()
+			assert.NotNil(t, result)
+			defer result.Body.Close()
+
+			bodyBytes, err := io.ReadAll(result.Body)
+			assert.Nil(err)
+
+			bodyString := string(bodyBytes)
+			assert.Equal(test.ExpectedResult, bodyString)
+			assert.Equal(result.StatusCode, test.ExpectedStatusCode)
+		})
+	}
+}
+
+func TestGetMSTeamsTeamChannels(t *testing.T) {
+	for _, test := range []struct {
+		Name               string
+		SetupPlugin        func(*plugintest.API)
+		SetupStore         func(*storemocks.Store)
+		SetupClient        func(*clientmocks.Client)
+		QueryParamTeamID   string
+		ExpectedResult     string
+		ExpectedStatusCode int
+	}{
+		{
+			Name: "TestGetMSTeamsTeamChannels: MS Teams team channels listed successfully",
+			SetupPlugin: func(api *plugintest.API) {
+				api.On("GetConfig").Return(&model.Config{ServiceSettings: model.ServiceSettings{SiteURL: model.NewString("/")}}, nil).Times(1)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetUserID()).Return(&oauth2.Token{}, nil).Times(1)
+			},
+			SetupClient: func(c *clientmocks.Client) {
+				c.On("ListChannels", testutils.GetTeamsTeamID()).Return([]*clientmodels.Channel{
+					{
+						ID:          "mockTeamsChannelID-1",
+						DisplayName: "mockDisplayName-1",
+						Description: "mockDescription-1",
+					},
+					{
+						ID:          "mockTeamsChannelID-2",
+						DisplayName: "mockDisplayName-2",
+						Description: "mockDescription-2",
+					},
+				}, nil).Times(1)
+			},
+			QueryParamTeamID:   testutils.GetTeamsTeamID(),
+			ExpectedResult:     `[{"ID":"mockTeamsChannelID-1","DisplayName":"mockDisplayName-1","Description":"mockDescription-1"},{"ID":"mockTeamsChannelID-2","DisplayName":"mockDisplayName-2","Description":"mockDescription-2"}]`,
+			ExpectedStatusCode: http.StatusOK,
+		},
+		{
+			Name: "TestGetMSTeamsTeamChannels: error occurred while getting MS Teams team channels",
+			SetupPlugin: func(api *plugintest.API) {
+				api.On("GetConfig").Return(&model.Config{ServiceSettings: model.ServiceSettings{SiteURL: model.NewString("/")}}, nil).Times(1)
+				api.On("LogError", "Unable to get the channels for MS Teams team", "TeamID", testutils.GetTeamsTeamID(), "Error", "error occurred while getting MS Teams team channels").Times(1)
+			},
+			SetupStore: func(store *storemocks.Store) {
+				store.On("GetTokenForMattermostUser", testutils.GetUserID()).Return(&oauth2.Token{}, nil).Times(1)
+			},
+			SetupClient: func(c *clientmocks.Client) {
+				c.On("ListChannels", testutils.GetTeamsTeamID()).Return(nil, errors.New("error occurred while getting MS Teams team channels")).Times(1)
+			},
+			QueryParamTeamID:   testutils.GetTeamsTeamID(),
+			ExpectedResult:     "Error occurred while fetching the MS Teams team channels.\n",
+			ExpectedStatusCode: http.StatusInternalServerError,
+		},
+	} {
+		t.Run(test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			plugin := newTestPlugin(t)
+			mockAPI := &plugintest.API{}
+
+			plugin.SetAPI(mockAPI)
+			defer mockAPI.AssertExpectations(t)
+
+			test.SetupPlugin(mockAPI)
+			test.SetupStore(plugin.store.(*storemocks.Store))
+			test.SetupClient(plugin.clientBuilderWithToken("", "", "", "", nil, nil).(*clientmocks.Client))
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/msteams/teams/%s/channels", testutils.GetTeamsTeamID()), nil)
 			r.Header.Add(HeaderMattermostUserID, testutils.GetUserID())
 			queryParams := url.Values{
 				QueryParamPerPage: {fmt.Sprint(DefaultPerPageLimit)},

server/get_teams_data.go
@@ -107,6 +107,28 @@ func (p *Plugin) GetMSTeamsTeamList(userID string, r *http.Request) ([]*clientmo
 	return teams, http.StatusOK, nil
 }
 
+func (p *Plugin) GetMSTeamsTeamChannels(teamID, userID string, r *http.Request) ([]*clientmodels.Channel, int, error) {
+	var client msteams.Client
+	var err error
+	if r.Context().Value(ContextClientKey) == nil {
+		client, err = p.GetClientForUser(userID)
+		if err != nil {
+			p.API.LogError("Unable to get the client for user", "MMUserID", userID, "Error", err.Error())
+			return nil, http.StatusUnauthorized, err
+		}
+	} else {
+		client = r.Context().Value(ContextClientKey).(msteams.Client)
+	}
+
+	channels, err := client.ListChannels(teamID)
+	if err != nil {
+		p.API.LogError("Unable to get the channels for MS Teams team", "TeamID", teamID, "Error", err.Error())
+		return nil, http.StatusInternalServerError, err
+	}
+
+	return channels, http.StatusOK, nil
+}
+
 func (p *Plugin) GetOffsetAndLimit(query url.Values) (offset, limit int) {
 	var page int
 	if val, err := strconv.Atoi(query.Get(QueryParamPage)); err != nil || val < 0 {

server/msteams/mocks/Client.go
@@ -473,15 +473,15 @@ func (_m *Client) GetUserAvatar(userID string) ([]byte, error) {
 }
 
 // ListChannels provides a mock function with given fields: teamID
-func (_m *Client) ListChannels(teamID string) ([]clientmodels.Channel, error) {
+func (_m *Client) ListChannels(teamID string) ([]*clientmodels.Channel, error) {
 	ret := _m.Called(teamID)
 
-	var r0 []clientmodels.Channel
-	if rf, ok := ret.Get(0).(func(string) []clientmodels.Channel); ok {
+	var r0 []*clientmodels.Channel
+	if rf, ok := ret.Get(0).(func(string) []*clientmodels.Channel); ok {
 		r0 = rf(teamID)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).([]clientmodels.Channel)
+			r0 = ret.Get(0).([]*clientmodels.Channel)
 		}
 	}
 