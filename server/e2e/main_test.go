1bbd510442ed108351a39b94c65f9a5e48db1646
Jesús Espino <jespinog@gmail.com>
2023-11-15 18:47:38 +0100

    Adding initial test to verify re-sync on reconnect

--- server/e2e/main_test.go
+++ server/e2e/main_test.go
@@ -91,6 +91,72 @@ func TestSendMessageAndReplyToMSTeamsLinkedChannel(t *testing.T) {
 	require.Equal(t, mattermostNewReply.RootId, mattermostNewMessage.Id)
 }
 
+func TestStopPluginSendMessageAndReplyToMSTeamsLinkedChannelStartPlugin(t *testing.T) {
+	setup(t)
+
+	t.Log("Disabling the plugin")
+	_, err := mmClientAdmin.DisablePlugin(context.Background(), "com.mattermost.msteams-sync")
+	if err != nil {
+		t.Fatal("Unable to disable the plugin", err)
+	}
+	defer mmClientAdmin.EnablePlugin(context.Background(), "com.mattermost.msteams-sync")
+
+	generatedMessage := uuid.New().String()
+	generatedReply := uuid.New().String()
+
+	t.Log("Sending messages to MSTeams")
+	newMessage, err := msClient.SendMessage(testCfg.MSTeams.ConnectedChannelTeamId, testCfg.MSTeams.ConnectedChannelId, "", generatedMessage)
+	require.NoError(t, err)
+
+	_, err = msClient.SendMessage(testCfg.MSTeams.ConnectedChannelTeamId, testCfg.MSTeams.ConnectedChannelId, newMessage.ID, generatedReply)
+	require.NoError(t, err)
+
+	time.Sleep(3 * time.Second)
+
+	t.Log("Verifying that the messages hasn't been synced to Mattermost")
+	posts, _, err := mmClient.GetPostsForChannel(context.Background(), testCfg.Mattermost.ConnectedChannelId, 0, 10, "", false, false)
+	require.NoError(t, err)
+
+	var mattermostNewMessage *model.Post
+	var mattermostNewReply *model.Post
+	for _, post := range posts.Posts {
+		if post.Message == generatedMessage {
+			mattermostNewMessage = post
+		}
+		if post.Message == generatedReply {
+			mattermostNewReply = post
+		}
+	}
+
+	require.Nil(t, mattermostNewMessage)
+	require.Nil(t, mattermostNewReply)
+
+	t.Log("Enabling the plugin")
+	_, err = mmClientAdmin.EnablePlugin(context.Background(), "com.mattermost.msteams-sync")
+	if err != nil {
+		t.Fatal("Unable to re-enable the plugin", err)
+	}
+
+	t.Log("Waiting for the plugin to and sync")
+	time.Sleep(5 * time.Second)
+
+	posts, _, err = mmClient.GetPostsForChannel(context.Background(), testCfg.Mattermost.ConnectedChannelId, 0, 10, "", false, false)
+	require.NoError(t, err)
+
+	for _, post := range posts.Posts {
+		if post.Message == generatedMessage {
+			mattermostNewMessage = post
+		}
+		if post.Message == generatedReply {
+			mattermostNewReply = post
+		}
+	}
+
+	require.NotNil(t, mattermostNewMessage)
+	require.NotNil(t, mattermostNewReply)
+	require.Equal(t, mattermostNewReply.RootId, mattermostNewMessage.Id)
+}
+
 func TestSendMessageAndReplyToMattermostLinkedChannel(t *testing.T) {
 	setup(t)
 	startTime := time.Now()
