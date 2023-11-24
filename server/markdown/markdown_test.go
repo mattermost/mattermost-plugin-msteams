ceb5d7b51a1c6f1eb08d7ee92d6fc9a8f787f428
Manoj <manoj@brightscout.com>
2023-11-24 21:06:33 +0530

    [MI-3768] Added unit tests for covering heading tags in the ConvertToMD function

--- server/markdown/markdown_test.go
+++ server/markdown/markdown_test.go
@@ -18,6 +18,11 @@ func TestConvertToMD(t *testing.T) {
 			expectedOutput: "This is text area <></>",
 		},
 		{
+			description:    "Text contains tags which shouldn't be converted to markdown",
+			text:           "This is text area <textarea></textarea>",
+			expectedOutput: "This is text area <textarea></textarea>",
+		},
+		{
 			description:    "Text contains div and paragraph tags",
 			text:           "This is text area with <div> and <p> tags",
 			expectedOutput: "This is text area with \n and \n tags\n\n\n\n\n",
@@ -26,6 +31,11 @@ func TestConvertToMD(t *testing.T) {
 			description:    "Text contains paragraph and image tags",
 			text:           "This is text area with <img src=''> and <p class=''>Paragraph</p> tags",
 			expectedOutput: "This is text area with ![]() and \nParagraph\n\n\n tags\n",
+		},
+		{
+			description:    "Text contains heading tags",
+			text:           "This is text area with <h1>H1</h1>, <h2>H2</h2> and <h3>H3</h3> tags",
+			expectedOutput: "This is text area with \n# H1\n\n, \n## H2\n\n and \n### H3\n\n tags\n",
 		},
 	} {
 		t.Run(testCase.description, func(t *testing.T) {
