2faaa417b1b40db562dc51c21d4609c80f4704f8
Jesse Hallam <jesse.hallam@gmail.com>
2023-12-13 10:12:59 -0400

    Revert "update manitest.test.tsx"
    This reverts commit 436f832771a467b96ba604ce85a5eb32ba898444.

--- webapp/src/manifest.test.tsx
+++ webapp/src/manifest.test.tsx
@@ -1,9 +1,15 @@
-import manifest from './manifest';
+import manifest, {id, version} from './manifest';
 
 describe('manifest module', () => {
     test('Plugin manifest, id and version are defined', () => {
         expect(manifest).toBeDefined();
         expect(manifest.id).toBeDefined();
         expect(manifest.version).toBeDefined();
+    });
+
+    // To ease migration, verify separate export of id and version.
+    test('Plugin id and version are defined', () => {
+        expect(id).toBeDefined();
+        expect(version).toBeDefined();
     });
 });
