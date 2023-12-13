436f832771a467b96ba604ce85a5eb32ba898444
Jesse Hallam <jesse.hallam@gmail.com>
2023-12-13 10:08:36 -0400

    update manitest.test.tsx

--- webapp/src/manifest.test.tsx
+++ webapp/src/manifest.test.tsx
@@ -1,15 +1,9 @@
-import manifest, {id, version} from './manifest';
+import manifest from './manifest';
 
 describe('manifest module', () => {
     test('Plugin manifest, id and version are defined', () => {
         expect(manifest).toBeDefined();
         expect(manifest.id).toBeDefined();
         expect(manifest.version).toBeDefined();
-    });
-
-    // To ease migration, verify separate export of id and version.
-    test('Plugin id and version are defined', () => {
-        expect(id).toBeDefined();
-        expect(version).toBeDefined();
     });
 });
