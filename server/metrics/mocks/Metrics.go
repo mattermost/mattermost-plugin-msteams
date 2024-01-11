55a974b53ba8440d64832b84913eaa43379f4c0f
Jesús Espino <jespinog@gmail.com>
2024-01-11 22:03:58 +0100

    Adding whitelist limit metric (#460)
    * Adding whitelist limit metric
    
    * Regenerating mocks
    
    * Update server/metrics/metrics.go
    
    Co-authored-by: Jesse Hallam <jesse.hallam@gmail.com>
    
    * Fixing a couple of small problems with the metric
    
    * Addressing PR review comments
    
    ---------
    
    Co-authored-by: Jesse Hallam <jesse.hallam@gmail.com>

--- server/metrics/mocks/Metrics.go
+++ server/metrics/mocks/Metrics.go
@@ -22,22 +22,6 @@ func (_m *Metrics) DecrementChangeEventQueueLength(changeType string) {
 	_m.Called(changeType)
 }
 
-// ObserveWorker provides a mock function with given fields: worker
-func (_m *Metrics) ObserveWorker(worker string) func() {
-	ret := _m.Called(worker)
-
-	var r0 func()
-	if rf, ok := ret.Get(0).(func(string) func()); ok {
-		r0 = rf(worker)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(func())
-		}
-	}
-
-	return r0
-}
-
 // GetRegistry provides a mock function with given fields:
 func (_m *Metrics) GetRegistry() *prometheus.Registry {
 	ret := _m.Called()
@@ -162,6 +146,27 @@ func (_m *Metrics) ObserveSyntheticUsers(count int64) {
 // ObserveUpstreamUsers provides a mock function with given fields: count
 func (_m *Metrics) ObserveUpstreamUsers(count int64) {
 	_m.Called(count)
+}
+
+// ObserveWhitelistLimit provides a mock function with given fields: limit
+func (_m *Metrics) ObserveWhitelistLimit(limit int) {
+	_m.Called(limit)
+}
+
+// ObserveWorker provides a mock function with given fields: worker
+func (_m *Metrics) ObserveWorker(worker string) func() {
+	ret := _m.Called(worker)
+
+	var r0 func()
+	if rf, ok := ret.Get(0).(func(string) func()); ok {
+		r0 = rf(worker)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(func())
+		}
+	}
+
+	return r0
 }
 
 // ObserveWorkerDuration provides a mock function with given fields: worker, elapsed
