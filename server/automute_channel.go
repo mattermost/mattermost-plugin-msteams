8fd4364857e614c55c3cd89e5584a4b8966dc801
Harrison Healey <harrisonmhealey@gmail.com>
2024-02-06 16:28:07 -0500

    Have AutomuteAPIMock call hooks when creating a channel

--- server/automute_channel.go
+++ server/automute_channel.go
@@ -8,3 +8,7 @@ import (
 func (p *Plugin) UserHasJoinedChannel(c *plugin.Context, channelMember *model.ChannelMember, actor *model.User) {
 	// TODO MM-56498
 }
+
+func (p *Plugin) ChannelHasBeenCreated(c *plugin.Context, channel *model.Channel) {
+	// TODO MM-56499
+}
