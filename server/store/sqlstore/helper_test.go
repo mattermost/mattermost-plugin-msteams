370248d9aea0cefde25ebd5faedb5d5089789e3c
Jesse Hallam <jesse.hallam@gmail.com>
2024-02-16 16:29:51 -0400

    avoid burying errors during test setup (#502)

--- server/store/sqlstore/helper_test.go
+++ server/store/sqlstore/helper_test.go
@@ -9,27 +9,32 @@ import (
 	"testing"
 
 	"github.com/jmoiron/sqlx"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
-
 	"github.com/mattermost/mattermost/server/public/model"
 	"github.com/mattermost/mattermost/server/public/plugin/plugintest"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 var db *sql.DB
 
 func TestMain(m *testing.M) {
 	var tearDown func()
-	db, tearDown = createTestDB()
+	var err error
+	db, tearDown, err = createTestDB()
+	if err != nil {
+		panic("failed to create test db: " + err.Error())
+	}
 	retCode := m.Run()
 	tearDown()
 
 	os.Exit(retCode)
 }
 
-func createTestDB() (*sql.DB, func()) {
+func createTestDB() (*sql.DB, func(), error) {
 	context := context.Background()
-	postgres, _ := testcontainers.GenericContainer(context,
+	postgres, err := testcontainers.GenericContainer(context,
 		testcontainers.GenericContainerRequest{
 			ContainerRequest: testcontainers.ContainerRequest{
 				Image:        "postgres",
@@ -45,13 +50,22 @@ func createTestDB() (*sql.DB, func()) {
 			},
 			Started: true,
 		})
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to create postgres container")
+	}
 
 	driverName := model.DatabaseDriverPostgres
-	host, _ := postgres.Host(context)
-	hostPort, _ := postgres.MappedPort(context, "5432/tcp")
+	host, err := postgres.Host(context)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to get postgres host")
+	}
+	hostPort, err := postgres.MappedPort(context, "5432/tcp")
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to map postgres port")
+	}
 	conn, err := sqlx.Connect(driverName, fmt.Sprintf("%s://user:pass@%s:%d?sslmode=disable", driverName, host, hostPort.Int()))
 	if err != nil {
-		panic("failed to connect to test db: " + err.Error())
+		return nil, nil, errors.Wrap(err, "failed to connect to test db")
 	}
 	tearDownContainer := func() {
 		if err := postgres.Terminate(context); err != nil {
@@ -59,20 +73,24 @@ func createTestDB() (*sql.DB, func()) {
 		}
 	}
 
-	return conn.DB, tearDownContainer
+	return conn.DB, tearDownContainer, nil
 }
 
-func setupTestStore(_ *testing.T) (*SQLStore, *plugintest.API) {
+func setupTestStore(t *testing.T) (*SQLStore, *plugintest.API) {
 	api := &plugintest.API{}
 
 	store := &SQLStore{}
 	store.api = api
 	store.db = db
 
-	_ = store.Init()
-	_ = store.createTable("Teams", "Id VARCHAR(255), DisplayName VARCHAR(255)")
-	_ = store.createTable("Channels", "Id VARCHAR(255), DisplayName VARCHAR(255)")
-	_ = store.createTable("Users", "Id VARCHAR(255), FirstName VARCHAR(255), LastName VARCHAR(255), Email VARCHAR(255)")
+	err := store.Init()
+	require.NoError(t, err)
+	err = store.createTable("Teams", "Id VARCHAR(255), DisplayName VARCHAR(255)")
+	require.NoError(t, err)
+	err = store.createTable("Channels", "Id VARCHAR(255), DisplayName VARCHAR(255)")
+	require.NoError(t, err)
+	err = store.createTable("Users", "Id VARCHAR(255), FirstName VARCHAR(255), LastName VARCHAR(255), Email VARCHAR(255)")
+	require.NoError(t, err)
 
 	return store, api
 }
