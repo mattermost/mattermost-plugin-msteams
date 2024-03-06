8e578fea4e081011762672ae6be899696c6df78b
Scott Bishel <scott.bishel@mattermost.com>
2024-03-06 09:47:32 -0700

    change function name

--- server/api.go
+++ server/api.go
@@ -66,7 +66,7 @@ func NewAPI(p *Plugin, store store.Store) *API {
 	router.HandleFunc("/autocomplete/teams", api.autocompleteTeams).Methods("GET")
 	router.HandleFunc("/autocomplete/channels", api.autocompleteChannels).Methods("GET")
 	router.HandleFunc("/connect", api.connect).Methods("GET", "OPTIONS")
-	router.HandleFunc("/reconnect", api.anothercall).Methods("GET", "OPTIONS")
+	router.HandleFunc("/reconnect", api.reconnectt).Methods("GET", "OPTIONS")
 	router.HandleFunc("/oauth-redirect", api.oauthRedirectHandler).Methods("GET", "OPTIONS")
 	router.HandleFunc("/connected-users", api.getConnectedUsers).Methods(http.MethodGet)
 	router.HandleFunc("/connected-users/download", api.getConnectedUsersFile).Methods(http.MethodGet)
@@ -322,7 +322,7 @@ func (a *API) connect(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (a *API) anothercall(w http.ResponseWriter, r *http.Request) {
+func (a *API) reconnectt(w http.ResponseWriter, r *http.Request) {
 	connectURL := a.getConnectURL(w, r)
 	if connectURL != "" {
 		http.Redirect(w, r, connectURL, http.StatusSeeOther)
