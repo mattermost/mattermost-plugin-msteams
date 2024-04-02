ec96cc1bcaee72666790ebbfb1219fc029eff5bb
Jesse Hallam <jesse.hallam@gmail.com>
2024-04-02 16:04:10 -0300

    MM-57534: monitor message creation delay (#567)
    * remove unused confirmed message metric
    
    * MM-57534: monitor message creation delay
    
    Keep track of the delay between when a message is created in one platform and when it is recorded as such in the other. Note that this inherently includes clock skew betewen the two platforms, and so isn't necessarily a good indicator of absolute time delay.
    
    Fixes: https://mattermost.atlassian.net/browse/MM-57534

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -276,7 +276,6 @@ func (ah *ActivityHandler) handleCreatedActivity(msg *clientmodels.Message, subs
 	// Avoid possible duplication
 	postInfo, _ := ah.plugin.GetStore().GetPostInfoByMSTeamsID(msg.ChatID+msg.ChannelID, msg.ID)
 	if postInfo != nil {
-		ah.plugin.GetMetrics().ObserveConfirmedMessage(metrics.ActionSourceMattermost, isDirectMessage)
 		return metrics.DiscardedReasonDuplicatedPost
 	}
 
@@ -372,6 +371,7 @@ func (ah *ActivityHandler) handleCreatedActivity(msg *clientmodels.Message, subs
 	}
 
 	ah.plugin.GetMetrics().ObserveMessage(metrics.ActionCreated, metrics.ActionSourceMSTeams, isDirectMessage)
+	ah.plugin.GetMetrics().ObserveMessageDelay(metrics.ActionCreated, metrics.ActionSourceMSTeams, isDirectMessage, time.Since(msg.CreateAt))
 
 	if errorFound {
 		_ = ah.plugin.GetAPI().SendEphemeralPost(senderID, &model.Post{
