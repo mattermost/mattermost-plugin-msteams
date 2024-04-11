299adeb51604b77d69744294b27c3b951c5c8d2a
Caleb Roseland <caleb@calebroseland.com>
2024-04-11 11:35:11 -0500

    wip-4

--- webapp/src/components/admin_console/inviteWhitelistSetting.tsx
+++ webapp/src/components/admin_console/inviteWhitelistSetting.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 
 type Props = {
     label: string;
@@ -6,17 +6,69 @@ type Props = {
 };
 
 const InviteWhitelistSetting = ({label}: Props) => {
-    return (
-        <div
-            className='form-group'
-        >
-            <label className='control-label col-sm-4'>{label}</label>
-            <div className='col-sm-8'>
-                <p>
-                    {'Upload a CSV file containing mattermost users that will receive invites to connect to MS Teams'}
-                </p>
-            </div>
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [pendingFile, setPendingFile] = useState<File>();
 
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+
+        if (file) {
+            setPendingFile(file);
+        }
+    };
+
+    const doUpload = () => {
+
+    };
+
+    return (
+        <div className='form-group'>
+            <label className='control-label col-sm-4'>
+                {label}
+            </label>
+            <div className='col-sm-8'>
+                <div className='file__upload'>
+                    <button
+                        type='button'
+                        className={'btn btn-tertiary'}
+                    >
+                        {'Choose file'}
+                    </button>
+                    <input
+                        ref={fileInputRef}
+                        type='file'
+                        accept='.csv'
+                        onChange={onChange}
+                    />
+                </div>
+                <button
+                    className={'btn btn-primary'}
+                    id='uploadWhitelistCsv'
+                    disabled={!pendingFile}
+                    onClick={doUpload}
+                >
+                    {'Upload'}
+                </button>
+
+                <div className='help-text m-0'>
+                    {pendingFile?.name}
+                </div>
+
+                <p className='help-text'>
+                    {'Upload a CSV file containing mattermost user-emails that may receive invites to connect to MS Teams. NOTE: This will replace the entire whitelist, be sure it is complete before uploading.'}
+                </p>
+
+                <p className='help-text'>
+                    {'To view the current whitelist: '}
+                    <button
+                        type='button'
+                        className={'btn btn-primary btn-sm'}
+                    >
+                        {'Download Whitelist'}
+                    </button>
+                </p>
+
+            </div>
         </div>
     );
 };
