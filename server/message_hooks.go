c9000aa1973b92e6112da6398d0fbb51ec13ed46
Scott Bishel <scott.bishel@mattermost.com>
2024-04-16 15:36:47 -0600

    MM-57563 - update method to allow self posts to sync, only if connected (#583)
    * update method to allow self posts to sync, only if connected
    
    * clean up function name
    
    * fix comment
    
    * add unit test
    
    * throw error if ChatMembersSpanPlatPlatforms only has single user
    
    * fix unit test
    
    * Update selective_sync_test.go
    
    reenable test

--- server/message_hooks.go
+++ server/message_hooks.go
@@ -60,7 +60,7 @@ func (p *Plugin) MessageHasBeenDeleted(_ *plugin.Context, post *model.Post) {
 		}
 
 		if p.getConfiguration().SelectiveSync {
-			shouldSync, appErr := p.ChatSpansPlatforms(post.ChannelId)
+			shouldSync, appErr := p.ChatShouldSync(post.ChannelId)
 			if appErr != nil {
 				p.API.LogWarn("Failed to check if chat should be synced", "error", appErr.Error(), "post_id", post.Id, "channel_id", post.ChannelId)
 				return
@@ -119,14 +119,18 @@ func (p *Plugin) MessageHasBeenPosted(_ *plugin.Context, post *model.Post) {
 			return
 		}
 
-		chatMembersSpanPlatforms, appErr := p.ChatMembersSpanPlatforms(members)
-		if appErr != nil {
-			p.API.LogWarn("Failed to check if chat members span platforms", "error", appErr.Error(), "post_id", post.Id, "channel_id", post.ChannelId)
-			return
-		}
+		isSelfPost := len(members) == 1
+		chatMembersSpanPlatforms := false
+		if !isSelfPost {
+			chatMembersSpanPlatforms, appErr = p.ChatMembersSpanPlatforms(members)
+			if appErr != nil {
+				p.API.LogWarn("Failed to check if chat members span platforms", "error", appErr.Error(), "post_id", post.Id, "channel_id", post.ChannelId)
+				return
+			}
 
-		if p.getConfiguration().SelectiveSync && !chatMembersSpanPlatforms {
-			return
+			if p.getConfiguration().SelectiveSync && !chatMembersSpanPlatforms {
+				return
+			}
 		}
 
 		dstUsers := []string{}
