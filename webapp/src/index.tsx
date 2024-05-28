15525d85da06450af28fce060d3528e6c319b349
Caleb Roseland <caleb@calebroseland.com>
2024-05-28 14:39:17 -0500

    remove invited and whitelist counts from site stats page

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -72,18 +72,6 @@ export default class Plugin {
                         icon: 'fa-users', // font-awesome-4.7.0 handler
                         value: siteStats?.total_connected_users || 0,
                     },
-                    msteams_invited_users: {
-                        name: 'MS Teams: Invited Users',
-                        id: 'msteams_invited_users',
-                        icon: 'fa-users', // font-awesome-4.7.0 handler
-                        value: siteStats?.pending_invited_users || 0,
-                    },
-                    msteams_whitelisted_users: {
-                        name: 'MS Teams: Whitelisted Users',
-                        id: 'msteams_whitelisted_users',
-                        icon: 'fa-users', // font-awesome-4.7.0 handler
-                        value: siteStats?.current_whitelist_users || 0,
-                    },
                     msteams_users_sending: {
                         name: 'MS Teams: Users sending',
                         id: 'msteams_users_sending',
