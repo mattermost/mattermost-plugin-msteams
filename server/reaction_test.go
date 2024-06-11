0157ba896ccc740bfc4ad3d6f20cd363fe5497b6
Jesse Hallam <jesse.hallam@gmail.com>
2024-06-11 17:25:12 -0300

    migrate TestUnsetChatReaction

--- server/reaction_test.go
+++ server/reaction_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"math"
 	"testing"
 	"time"
 
@@ -201,147 +200,60 @@ func TestSetReaction(t *testing.T) {
 }
 
 func TestUnsetChatReaction(t *testing.T) {
-	mockChat := &clientmodels.Chat{
-		ID: testutils.GetChatID(),
-		Members: []clientmodels.ChatMember{
-			{
-				DisplayName: "mockDisplayName",
-				UserID:      testutils.GetTeamsUserID(),
-				Email:       testutils.GetTestEmail(),
-			},
-		},
+	th := setupTestHelper(t)
+	team := th.SetupTeam(t)
+
+	setupChat := func(t *testing.T, emojiName string) (*model.User, *model.Channel, string, string) {
+		th.Reset(t)
+
+		senderUser := th.SetupUser(t, team)
+
+		user2 := th.SetupUser(t, team)
+		th.ConnectUser(t, user2.Id)
+
+		channel, err := th.p.apiClient.Channel.GetDirect(senderUser.Id, user2.Id)
+		require.NoError(t, err)
+
+		chatID := model.NewId()
+		messageID := model.NewId()
+		mockTeamsHelper := newMockTeamsHelper(th)
+		mockTeamsHelper.registerChat(chatID, []*model.User{senderUser, user2})
+		mockTeamsHelper.registerChatMessage(chatID, messageID, senderUser, "message")
+
+		return senderUser, channel, chatID, messageID
 	}
-	mockChatMessage := &clientmodels.Message{
-		ID:           "mockTeamsMessageID",
-		ChatID:       testutils.GetChatID(),
-		LastUpdateAt: testutils.GetMockTime(),
-	}
-	for _, test := range []struct {
-		Name            string
-		SetupAPI        func(*plugintest.API)
-		SetupStore      func(*storemocks.Store)
-		SetupClient     func(*clientmocks.Client, *clientmocks.Client)
-		SetupMetrics    func(mockmetrics *metricsmocks.Metrics)
-		ExpectedMessage string
-	}{
-		{
-			Name:     "UnsetChatReaction: Unable to get the source user ID",
-			SetupAPI: func(api *plugintest.API) {},
-			SetupStore: func(store *storemocks.Store) {
-				store.On("MattermostToTeamsUserID", testutils.GetID()).Return("", testutils.GetInternalServerAppError("unable to get the source user ID")).Times(1)
-			},
-			SetupClient:     func(client *clientmocks.Client, uclient *clientmocks.Client) {},
-			SetupMetrics:    func(mockmetrics *metricsmocks.Metrics) {},
-			ExpectedMessage: "unable to get the source user ID",
-		},
-		{
-			Name:     "UnsetChatReaction: Unable to get the client",
-			SetupAPI: func(api *plugintest.API) {},
-			SetupStore: func(store *storemocks.Store) {
-				store.On("MattermostToTeamsUserID", testutils.GetID()).Return(testutils.GetID(), nil).Times(1)
-				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(nil, nil).Times(1)
-			},
-			SetupClient:     func(client *clientmocks.Client, uclient *clientmocks.Client) {},
-			SetupMetrics:    func(mockmetrics *metricsmocks.Metrics) {},
-			ExpectedMessage: "not connected user",
-		},
-		{
-			Name: "UnsetChatReaction: Unable to get the chat ID",
-			SetupAPI: func(api *plugintest.API) {
-				api.On("GetChannel", testutils.GetChannelID()).Return(nil, testutils.GetInternalServerAppError("unable to get the channel")).Times(1)
-			},
-			SetupStore: func(store *storemocks.Store) {
-				store.On("MattermostToTeamsUserID", testutils.GetID()).Return(testutils.GetID(), nil).Times(1)
-				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&fakeToken, nil).Times(1)
-			},
-			SetupClient:     func(client *clientmocks.Client, uclient *clientmocks.Client) {},
-			SetupMetrics:    func(mockmetrics *metricsmocks.Metrics) {},
-			ExpectedMessage: "unable to get the channel",
-		},
-		{
-			Name: "UnsetChatReaction: Unable to unset the chat reaction",
-			SetupAPI: func(api *plugintest.API) {
-				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeDirect), nil).Times(1)
-				api.On("GetChannelMembers", testutils.GetChannelID(), 0, math.MaxInt32).Return(testutils.GetChannelMembers(2), nil).Times(1)
-				api.On("GetConfig").Return(&model.Config{ServiceSettings: model.ServiceSettings{SiteURL: model.NewString("/")}}, nil).Times(2)
-				api.On("KVSetWithOptions", "mutex_post_mutex_"+testutils.GetChatID()+"mockTeamsMessageID", mock.Anything, mock.Anything).Return(true, nil).Times(2)
-			},
-			SetupStore: func(store *storemocks.Store) {
-				store.On("MattermostToTeamsUserID", testutils.GetID()).Return(testutils.GetID(), nil).Times(3)
-				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&fakeToken, nil).Times(2)
-			},
-			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
-				uclient.On("CreateOrGetChatForUsers", mock.AnythingOfType("[]string")).Return(mockChat, nil).Times(1)
-				uclient.On("UnsetChatReaction", testutils.GetChatID(), "mockTeamsMessageID", testutils.GetID(), ":mockEmojiName:").Return(nil, errors.New("unable to unset the chat reaction")).Times(1)
-			},
-			SetupMetrics: func(metrics *metricsmocks.Metrics) {
-				metrics.On("ObserveMSGraphClientMethodDuration", "Client.CreateOrGetChatForUsers", "true", "2XX", mock.AnythingOfType("float64")).Once()
-				metrics.On("ObserveMSGraphClientMethodDuration", "Client.UnsetChatReaction", "false", "0", mock.AnythingOfType("float64")).Once()
-			},
-			ExpectedMessage: "unable to unset the chat reaction",
-		},
-		{
-			Name: "UnsetChatReaction: Unable to set the post last updateAt time",
-			SetupAPI: func(api *plugintest.API) {
-				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeDirect), nil).Times(1)
-				api.On("GetChannelMembers", testutils.GetChannelID(), 0, math.MaxInt32).Return(testutils.GetChannelMembers(2), nil).Times(1)
-				api.On("GetConfig").Return(&model.Config{ServiceSettings: model.ServiceSettings{SiteURL: model.NewString("/")}}, nil).Times(2)
-				api.On("KVSetWithOptions", "mutex_post_mutex_"+testutils.GetChatID()+"mockTeamsMessageID", mock.Anything, mock.Anything).Return(true, nil).Times(2)
-			},
-			SetupStore: func(store *storemocks.Store) {
-				store.On("MattermostToTeamsUserID", testutils.GetID()).Return(testutils.GetID(), nil).Times(3)
-				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&fakeToken, nil).Times(2)
-				store.On("SetPostLastUpdateAtByMSTeamsID", "mockTeamsMessageID", testutils.GetMockTime()).Return(errors.New("unable to set post lastUpdateAt value")).Once()
-			},
-			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
-				uclient.On("CreateOrGetChatForUsers", mock.AnythingOfType("[]string")).Return(mockChat, nil).Times(1)
-				uclient.On("UnsetChatReaction", testutils.GetChatID(), "mockTeamsMessageID", testutils.GetID(), ":mockEmojiName:").Return(mockChatMessage, nil).Times(1)
-			},
-			SetupMetrics: func(mockmetrics *metricsmocks.Metrics) {
-				mockmetrics.On("ObserveReaction", metrics.ReactionUnsetAction, metrics.ActionSourceMattermost, true).Times(1)
-				mockmetrics.On("ObserveMSGraphClientMethodDuration", "Client.CreateOrGetChatForUsers", "true", "2XX", mock.AnythingOfType("float64")).Once()
-				mockmetrics.On("ObserveMSGraphClientMethodDuration", "Client.UnsetChatReaction", "true", "2XX", mock.AnythingOfType("float64")).Once()
-			},
-		},
-		{
-			Name: "UnsetChatReaction: Valid",
-			SetupAPI: func(api *plugintest.API) {
-				api.On("GetChannel", testutils.GetChannelID()).Return(testutils.GetChannel(model.ChannelTypeDirect), nil).Times(1)
-				api.On("GetChannelMembers", testutils.GetChannelID(), 0, math.MaxInt32).Return(testutils.GetChannelMembers(2), nil).Times(1)
-				api.On("GetConfig").Return(&model.Config{ServiceSettings: model.ServiceSettings{SiteURL: model.NewString("/")}}, nil).Times(2)
-				api.On("KVSetWithOptions", "mutex_post_mutex_"+testutils.GetChatID()+"mockTeamsMessageID", mock.Anything, mock.Anything).Return(true, nil).Times(2)
-			},
-			SetupStore: func(store *storemocks.Store) {
-				store.On("MattermostToTeamsUserID", testutils.GetID()).Return(testutils.GetID(), nil).Times(3)
-				store.On("GetTokenForMattermostUser", testutils.GetID()).Return(&fakeToken, nil).Times(2)
-				store.On("SetPostLastUpdateAtByMSTeamsID", "mockTeamsMessageID", testutils.GetMockTime()).Return(nil).Once()
-			},
-			SetupClient: func(client *clientmocks.Client, uclient *clientmocks.Client) {
-				uclient.On("CreateOrGetChatForUsers", mock.AnythingOfType("[]string")).Return(mockChat, nil).Times(1)
-				uclient.On("UnsetChatReaction", testutils.GetChatID(), "mockTeamsMessageID", testutils.GetID(), ":mockEmojiName:").Return(mockChatMessage, nil).Times(1)
-			},
-			SetupMetrics: func(mockmetrics *metricsmocks.Metrics) {
-				mockmetrics.On("ObserveReaction", metrics.ReactionUnsetAction, metrics.ActionSourceMattermost, true).Times(1)
-				mockmetrics.On("ObserveMSGraphClientMethodDuration", "Client.CreateOrGetChatForUsers", "true", "2XX", mock.AnythingOfType("float64")).Once()
-				mockmetrics.On("ObserveMSGraphClientMethodDuration", "Client.UnsetChatReaction", "true", "2XX", mock.AnythingOfType("float64")).Once()
-			},
-		},
-	} {
-		t.Run(test.Name, func(t *testing.T) {
-			assert := assert.New(t)
-			p := newTestPlugin(t)
-			test.SetupAPI(p.API.(*plugintest.API))
-			test.SetupStore(p.store.(*storemocks.Store))
-			test.SetupClient(p.msteamsAppClient.(*clientmocks.Client), p.clientBuilderWithToken("", "", "", "", nil, nil).(*clientmocks.Client))
-			test.SetupMetrics(p.metricsService.(*metricsmocks.Metrics))
-			resp := p.UnsetChatReaction("mockTeamsMessageID", testutils.GetID(), testutils.GetChannelID(), "mockEmojiName")
-			if test.ExpectedMessage != "" {
-				assert.Contains(resp.Error(), test.ExpectedMessage)
-			} else {
-				assert.Nil(resp)
-			}
-		})
-	}
+
+	t.Run("sender not connected", func(t *testing.T) {
+		emojiName := "tada"
+		senderUser, channel, messageID, _ := setupChat(t, emojiName)
+
+		err := th.p.UnsetChatReaction(messageID, senderUser.Id, channel.Id, emojiName)
+		require.ErrorIs(t, err, sql.ErrNoRows)
+	})
+
+	t.Run("failed to set the chat reaction", func(t *testing.T) {
+		emojiName := "tada"
+		senderUser, channel, chatID, messageID := setupChat(t, emojiName)
+		th.ConnectUser(t, senderUser.Id)
+
+		th.clientMock.On("UnsetChatReaction", chatID, messageID, "t"+senderUser.Id, "🎉").Return(nil, errors.New("unable to unset the chat reaction"))
+
+		err := th.p.UnsetChatReaction(messageID, senderUser.Id, channel.Id, emojiName)
+		require.Error(t, err)
+	})
+
+	t.Run("chat reaction removed", func(t *testing.T) {
+		emojiName := "tada"
+		senderUser, channel, chatID, messageID := setupChat(t, emojiName)
+		th.ConnectUser(t, senderUser.Id)
+
+		th.clientMock.On("UnsetChatReaction", chatID, messageID, "t"+senderUser.Id, "🎉").Return(&clientmodels.Message{
+			LastUpdateAt: time.Now(),
+		}, nil).Once()
+
+		err := th.p.UnsetChatReaction(messageID, senderUser.Id, channel.Id, emojiName)
+		require.NoError(t, err)
+	})
 }
 
 func TestUnsetReaction(t *testing.T) {
